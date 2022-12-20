363d7c8dc526a39b1135a794d0059895cea065d8
Bensalem Amine <bensalem.amine321@gmail.com>
2022-12-20 12:48:06 +0100

    [FIX] line numbers were not displayed correctly

--- scripts/todolist.sh
+++ scripts/todolist.sh
@@ -6,10 +6,12 @@ cd ..
 > TODOlist.txt
 
 for file in sources/*.cpp includes/*.h kernels/*.cu* shaders/*; do
-	TODO="$(cat $file | grep -e '//TODO' -e '//todo' | tr -d '\t')" 
-	LINE="$(cat $file | grep -n -e '//TODO' -e '//todo' | tr -d '\t' | cut -d ':' -f1)"
+	LINE="$(cat $file | grep -n -e '//TODO' -e '//todo' | tr -d '\t' )"
 	if ! [ -z "$LINE" ] ; then 
-		echo "$file:$LINE $TODO" >> TODOlist.txt
+		echo '' >> TODOlist.txt ; 
+		echo "$file:" >> TODOlist.txt ;
+		echo '' >> TODOlist.txt ;
+		echo "$LINE" >> TODOlist.txt
 	fi
 done 
 
