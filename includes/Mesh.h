9f67f331bf9540d469e1ab0f3c7c8d89541fa2e6
Bensalem Amine <bensalem.amine321@gmail.com>
2022-12-21 11:41:46 +0100

    [FEATURE] Adding names to meshes

--- includes/Mesh.h
+++ includes/Mesh.h
@@ -10,12 +10,13 @@ class Mesh{
 public:
 	Mesh();
 	Mesh(Object3D const& obj , Material const& mat); 
+	Mesh(std::string name , Object3D const& obj , Material const& mat); 
 	virtual ~Mesh(); 
 
 
 	Object3D geometry; 
 	Material material; 
-
+	std::string name; 
 
 
 
--- sources/Mesh.cpp
+++ sources/Mesh.cpp
@@ -12,6 +12,13 @@ Mesh::Mesh(Object3D const& geo , Material const& mat){
 	material = mat;
 }
 
+Mesh::Mesh(std::string n , Object3D const& geo , Material const& mat){
+	geometry = geo; 
+	material = mat;
+	name = n ; 
+}
+
+
 Mesh::~Mesh(){
 
 }
