92e672df3d782df96fa347122bb2d0ee724c915e
Bensalem Amine <bensalem.amine321@gmail.com>
2023-03-27 02:58:35 +0100

    [UPDATE] removed basic texture struct

--- includes/utils_3D.h
+++ includes/utils_3D.h
@@ -4,6 +4,7 @@
 #include "constants.h" 
 
 
+
 /*TODO : Create classes , implement clean up */
 namespace axomae {
 
@@ -15,59 +16,6 @@ struct Object3D{
 	std::vector<float> bitangents ; 
 	std::vector<float> tangents;
 	std::vector<unsigned int> indices;
-};
-
-struct TextureData{
-	unsigned int width ; 
-	unsigned int height ; 
-	std::string name ; 
-	uint32_t *data ; 
-		
-
-	TextureData(){
-		width = 0 ; 
-		height = 0 ; 
-		data = nullptr ; 
-	}	
-	~TextureData(){}
-	
-	/*provides deep copy of the object , but doesn't do the cleanup for the copied object*/
-	TextureData& operator=(const TextureData& from){ 
-		width = from.width ;
-		height = from.height ; 
-		data = new uint32_t [from.width * from.height] ; 
-		memcpy((void*) data , (void*) from.data , from.width * from.height * sizeof(uint32_t));		
-		name = from.name ; 
-		return *this ; 
-	}
-
-	void clean(){
-		if(data != nullptr)
-			delete data ; 
-		data = nullptr ;
-		width = 0 ; 
-		height = 0 ;
-		name = "" ; 
-
-	}
-};
-
-struct TexturePack{
-	TextureData diffuse ; 
-	TextureData normal ; 
-	TextureData opacity ; 
-	TextureData metallic ; 
-	TextureData roughness ;
-	TextureData optional ; 
-}; 
-
-//For PBR workflow
-struct Material{
-	TexturePack textures ;
-	float dielectric_factor; //metallic factor , 0.0 = full dielectric , 1.0 = full metallic
-	float roughtness_factor; //0.0 = smooth , 1.0 = rough 	
-	float transmission_factor; //defines amount of light transmitted through the surface
-	float emissive_factor;
 };
 
 
