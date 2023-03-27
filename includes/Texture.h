e9f3243cec02bab44d0f148674cc9d811d6b290e
Bensalem Amine <bensalem.amine321@gmail.com>
2023-03-27 02:57:33 +0100

    [ADD] PBR textures classes

--- includes/Texture.h
+++ includes/Texture.h
@@ -3,18 +3,136 @@
 
 #include "constants.h" 
 #include "utils_3D.h" 
+#include <QOpenGLTexture> 
+
+class TextureData{
+public:
+	unsigned int width ; 
+	unsigned int height ; 
+	std::string name ; 
+	uint32_t *data ; 
+	
+	TextureData(){
+		width = 0 ; 
+		height = 0 ; 
+		data = nullptr ; 
+	}
+
+	~TextureData(){}
+	
+	/*provides deep copy of the object , but doesn't do the cleanup for the copied object*/
+	TextureData& operator=(const TextureData& from){ 
+		width = from.width ;
+		height = from.height ; 
+		data = new uint32_t [from.width * from.height] ; 
+		memcpy((void*) data , (void*) from.data , from.width * from.height * sizeof(uint32_t));		
+		name = from.name ; 
+		return *this ; 
+	}
+
+	void clean(){
+		if(data != nullptr)
+			delete data ; 
+		data = nullptr ;
+		width = 0 ; 
+		height = 0 ;
+		name = "" ; 
+	}
+};
+
 
 class Texture{
 public:
-	Texture(); 
-	virtual ~Texture();
+	enum TYPE : unsigned {DIFFUSE = 0 , NORMAL = 1 , METALLIC = 2 , ROUGHNESS = 3 , AMBIANTOCCLUSION = 4 , GENERIC = 5} ; 
 	
+	Texture(); 
+	Texture(TextureData *tex); 
+	virtual ~Texture();
+	void set(TextureData *texture); 
+	void clean();
+	static Texture* constructTexture(TextureData &data , TYPE type); 	
+	virtual void bindTexture() = 0 ; 
+	virtual void unbindTexture() = 0;
+	virtual void setGlData() = 0 ; 
+	void cleanGlData(); 
 
-private:
-	const char* name ; 
-	unsigned int GL_id ; 
+protected:
+	std::string name ;
+	unsigned int width ; 
+	unsigned int height ; 
+	uint32_t *data ; 
+	unsigned int sampler2D ; 
 	
 };
+
+//Albedo texture
+class DiffuseTexture : public Texture{
+public:
+	DiffuseTexture();
+	DiffuseTexture(TextureData *data):Texture(data){}; 
+	virtual ~DiffuseTexture(); 
+	virtual void setGlData() ;
+	virtual void bindTexture() ; 
+	virtual void unbindTexture() ;
+	static const char* getTextureTypeCStr()   ; 	
+}; 
+
+
+class NormalTexture : public Texture{
+public:
+	NormalTexture();
+	NormalTexture(TextureData* data):Texture(data){} ; 
+	virtual ~NormalTexture(); 
+	virtual void setGlData() ;
+	virtual void bindTexture() ; 
+	virtual void unbindTexture() ;
+	static const char* getTextureTypeCStr()  ; 	
+};
+
+class MetallicTexture : public Texture{
+public:
+	MetallicTexture();
+	MetallicTexture(TextureData* data):Texture(data){}; 
+	virtual ~MetallicTexture(); 
+	virtual void setGlData() ;
+	virtual void bindTexture() ; 
+	virtual void unbindTexture();
+	static const char* getTextureTypeCStr()  ; 	
+};
+
+class RoughnessTexture : public Texture{
+public:
+	RoughnessTexture();
+	RoughnessTexture(TextureData *data):Texture(data){}; 
+	virtual ~RoughnessTexture(); 
+	virtual void setGlData() ;
+	virtual void bindTexture()  ; 
+	virtual void unbindTexture() ;
+	static const char* getTextureTypeCStr() ; 	
+};
+
+class AmbiantOcclusionTexture : public Texture{
+public:
+	AmbiantOcclusionTexture();
+	AmbiantOcclusionTexture(TextureData *data):Texture(data){}; 
+	virtual ~AmbiantOcclusionTexture(); 
+	virtual void setGlData() ;
+	virtual void bindTexture()  ; 
+	virtual void unbindTexture();
+	static  const char* getTextureTypeCStr() ; 	
+};
+
+class GenericTexture : public Texture{
+public:
+	GenericTexture();
+	GenericTexture(TextureData* data):Texture(data){}; 
+	virtual ~GenericTexture(); 
+	virtual void setGlData(); 
+	virtual void bindTexture()  ; 
+	virtual void unbindTexture() ;
+	static  const char* getTextureTypeCStr() ; 	
+}; 
+
 
 
 
--- sources/Texture.cpp
+++ sources/Texture.cpp
@@ -1,15 +1,328 @@
 #include "../includes/Texture.h"
+#include <map>
 
 
 
+static std::map<Texture::TYPE , const char*> texture_type_c_str = {
+	{Texture::DIFFUSE , "diffuse"}, 
+	{Texture::NORMAL , "normal"}, 
+	{Texture::METALLIC , "metallic"}, 
+	{Texture::ROUGHNESS , "roughness"}, 
+	{Texture::AMBIANTOCCLUSION , "ambiantocclusion"}, 
+	{Texture::GENERIC , "generic"}
+
+};
 
 Texture::Texture(){
+	name = "" ; 
+	width = 0 ; 
+	height = 0 ; 
+	data = nullptr ; 
+	sampler2D = 0 ; 
+}
 
-
+Texture::Texture(TextureData* tex){
+	name = "" ; 
+	width = 0 ; 
+	height = 0 ; 
+	data = nullptr ; 
+	sampler2D = 0 ; 
+	set(tex) ; 
 }
 
 
 Texture::~Texture(){
+	
+
+}
+
+void Texture::set(TextureData *texture){
+	clean(); 
+	name = texture->name; 
+	width = texture->width ; 
+	height = texture->height ; 
+	data = new uint32_t [ width * height ] ; 
+	for(unsigned int i = 0 ; i < width * height ; i++)
+		data[i] = texture->data[i] ; 
+
+}
+
+void Texture::clean(){
+	cleanGlData(); 
+	if(data != nullptr)
+		delete data; 
+	data = nullptr ; 
+	width = 0 ; 
+	height = 0 ; 
+	name = "" ; 
+
+}
+
+Texture* Texture::constructTexture(TextureData &T , TYPE type){
+	TextureData data ; 
+	data = T ; 
+	switch(type){
+		case DIFFUSE:
+			return new DiffuseTexture(&data); 
+		break; 
+		case NORMAL:
+			return new NormalTexture(&data);
+		break ; 
+		case METALLIC:
+			return new MetallicTexture(&data); 
+		break ; 
+		case ROUGHNESS:
+			return new RoughnessTexture(&data); 
+		break ; 
+		case AMBIANTOCCLUSION:
+			return new AmbiantOcclusionTexture(&data); 
+		break ; 
+		case GENERIC: 
+			return new GenericTexture(&data); 
+		break ; 
+		default : 
+			return nullptr ;
+		break ;
+	}
 
 
 }
+void Texture::cleanGlData(){
+	if(sampler2D != 0)
+		glDeleteTextures(1 , &sampler2D); 
+}
+
+	
+
+/****************************************************************************************************************************/
+DiffuseTexture::DiffuseTexture(){
+
+}
+
+DiffuseTexture::~DiffuseTexture(){
+
+}
+
+void DiffuseTexture::setGlData(){
+	glGenTextures(1 , &sampler2D); 	
+	glActiveTexture(GL_TEXTURE0 + DIFFUSE); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D); 
+	glTexImage2D(GL_TEXTURE_2D , 0 , GL_RGBA , width , height , 0 , GL_BGRA , GL_UNSIGNED_BYTE , data); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+}
+
+
+void DiffuseTexture::bindTexture(){
+	glActiveTexture(GL_TEXTURE0 + DIFFUSE); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D);
+}
+
+void DiffuseTexture::unbindTexture(){
+	glActiveTexture(GL_TEXTURE0 + DIFFUSE); 
+	glBindTexture(GL_TEXTURE_2D , 0);
+
+}
+
+const char* DiffuseTexture::getTextureTypeCStr() {
+	return texture_type_c_str[DIFFUSE] ; 		
+}
+
+/****************************************************************************************************************************/
+NormalTexture::NormalTexture(){
+
+}
+
+NormalTexture::~NormalTexture(){
+
+}
+
+void NormalTexture::setGlData(){
+	glGenTextures(1 , &sampler2D); 	
+	glActiveTexture(GL_TEXTURE0 + NORMAL); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D); 
+	glTexImage2D(GL_TEXTURE_2D , 0 , GL_RGBA , width , height , 0 , GL_BGRA , GL_UNSIGNED_BYTE , data); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+}
+
+
+void NormalTexture::bindTexture(){
+	glActiveTexture(GL_TEXTURE0 + NORMAL); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D);
+}
+
+void NormalTexture::unbindTexture(){
+	glActiveTexture(GL_TEXTURE0 + NORMAL); 
+	glBindTexture(GL_TEXTURE_2D , 0); 
+
+}
+
+const char* NormalTexture::getTextureTypeCStr()  {
+	return texture_type_c_str[NORMAL] ; 		
+}
+
+
+
+
+/****************************************************************************************************************************/
+MetallicTexture::MetallicTexture(){
+
+
+}
+
+MetallicTexture::~MetallicTexture(){
+
+}
+
+void MetallicTexture::setGlData(){
+	glGenTextures(1 , &sampler2D); 	
+	glActiveTexture(GL_TEXTURE0 + METALLIC); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D); 
+	glTexImage2D(GL_TEXTURE_2D , 0 , GL_RGBA , width , height , 0 , GL_BGRA , GL_UNSIGNED_BYTE , data); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+}
+
+
+void MetallicTexture::bindTexture(){
+	glActiveTexture(GL_TEXTURE0 + METALLIC); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D);
+}
+
+void MetallicTexture::unbindTexture(){
+	glActiveTexture(GL_TEXTURE0 + METALLIC); 
+	glBindTexture(GL_TEXTURE_2D , 0); 
+
+}
+
+const char* MetallicTexture::getTextureTypeCStr() {
+	return texture_type_c_str[METALLIC] ; 		
+}
+
+
+
+/****************************************************************************************************************************/
+RoughnessTexture::RoughnessTexture(){
+
+}
+
+RoughnessTexture::~RoughnessTexture(){
+
+}
+
+void RoughnessTexture::setGlData(){
+	glGenTextures(1 , &sampler2D); 	
+	glActiveTexture(GL_TEXTURE0 + ROUGHNESS); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D); 
+	glTexImage2D(GL_TEXTURE_2D , 0 , GL_RGBA , width , height , 0 , GL_BGRA , GL_UNSIGNED_BYTE , data); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+}
+
+void RoughnessTexture::bindTexture(){
+	glActiveTexture(GL_TEXTURE0 + ROUGHNESS); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D);
+}
+
+void RoughnessTexture::unbindTexture(){
+	glActiveTexture(GL_TEXTURE0 + ROUGHNESS); 
+	glBindTexture(GL_TEXTURE_2D , 0); 
+
+}
+
+const char* RoughnessTexture::getTextureTypeCStr()  {
+	return texture_type_c_str[ROUGHNESS] ; 		
+}
+
+
+
+/****************************************************************************************************************************/
+AmbiantOcclusionTexture::AmbiantOcclusionTexture(){
+
+}
+
+AmbiantOcclusionTexture::~AmbiantOcclusionTexture(){
+
+}
+
+void AmbiantOcclusionTexture::setGlData(){
+	glGenTextures(1 , &sampler2D); 	
+	glActiveTexture(GL_TEXTURE0 + AMBIANTOCCLUSION); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D); 
+	glTexImage2D(GL_TEXTURE_2D , 0 , GL_RGBA , width , height , 0 , GL_BGRA , GL_UNSIGNED_BYTE , data); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+}
+
+void AmbiantOcclusionTexture::bindTexture(){
+	glActiveTexture(GL_TEXTURE0 + AMBIANTOCCLUSION); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D);
+}
+
+void AmbiantOcclusionTexture::unbindTexture(){
+	glActiveTexture(GL_TEXTURE0 + AMBIANTOCCLUSION); 
+	glBindTexture(GL_TEXTURE_2D , 0); 
+
+}
+
+const char* AmbiantOcclusionTexture::getTextureTypeCStr()  {
+	return texture_type_c_str[AMBIANTOCCLUSION] ; 		
+}
+
+
+/****************************************************************************************************************************/
+
+GenericTexture::GenericTexture(){
+
+}
+
+GenericTexture::~GenericTexture(){
+
+}
+
+void GenericTexture::setGlData(){
+	glGenTextures(1 , &sampler2D); 	
+	glActiveTexture(GL_TEXTURE0 + GENERIC); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D); 
+	glTexImage2D(GL_TEXTURE_2D , 0 , GL_RGBA , width , height , 0 , GL_BGRA , GL_UNSIGNED_BYTE , data); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+}
+
+void GenericTexture::bindTexture(){
+	glActiveTexture(GL_TEXTURE0 + GENERIC); 
+	glBindTexture(GL_TEXTURE_2D , sampler2D);
+}
+
+void GenericTexture::unbindTexture(){
+	glActiveTexture(GL_TEXTURE0 + GENERIC); 
+	glBindTexture(GL_TEXTURE_2D , 0); 
+}
+
+
+const char* GenericTexture::getTextureTypeCStr() {
+	return texture_type_c_str[GENERIC] ; 		
+}
+
+
