33b303bb30b60287b55676c4d13b3864c5be72f4
Amine Bensalem <bensalem.amine321@gmail.com>
2023-04-20 07:43:50 +0100

    [FEATURE] Added a cubemap Mesh subtype

--- includes/Mesh.h
+++ includes/Mesh.h
@@ -10,6 +10,15 @@ namespace axomae {
 
 class Mesh{
 public:
+	enum DEPTHFUNC: GLenum { NEVER = GL_NEVER , 
+				LESS = GL_LESS    , 
+				EQUAL = GL_EQUAL  , 
+				LESS_OR_EQUAL = GL_LEQUAL , 
+				GREATER = GL_GREATER , 
+				GREATER_OR_EQUAL = GL_GEQUAL , 
+				NOT_EQUAL = GL_NOTEQUAL , 
+				ALWAYS = GL_ALWAYS} ;  
+
 	Mesh();
 	Mesh(const Mesh& copy); 
 	Mesh(Object3D const& obj , Material const& mat); 
@@ -22,31 +31,34 @@ public:
 	virtual bool isInitialized();
 	virtual void initializeGlData(); 
 	virtual void setSceneCameraPointer(Camera *camera); 
-
+	virtual void setRotationMatrix(glm::mat4 &rot){ model_matrix = rot ; }
+	void cullBackFace() ;
+	void cullFrontFace() ; 
+	void cullFrontAndBackFace() ; 
+	void setFaceCulling(bool value) ; 	
+	void setDepthMask(bool value);
+	void setDepthFunc(DEPTHFUNC func); 
 public:
 	Object3D geometry;
 	Shader shader_program; 
 	Material material; 
 	std::string name; 
 
-private:
+protected:
 	bool mesh_initialized ; 
 	Camera* camera; 
-
-
-
-
-
-
-
-
-
-
-
+	glm::mat4 model_matrix;
+	bool face_culling_enabled ;
+	bool depth_mask_enabled ; 
 };
 
 
-
+class CubeMapMesh : public Mesh{
+public:
+	CubeMapMesh(); 
+	virtual ~CubeMapMesh(); 
+	virtual void bindShaders(); 	
+};
 
 
 
--- sources/Mesh.cpp
+++ sources/Mesh.cpp
@@ -27,9 +27,8 @@ Mesh::Mesh(std::string n , Object3D const& geo , Material const& mat){
 	name = n ; 
 }
 
-Mesh::~Mesh(){
+Mesh::~Mesh(){}
 
-}
 void Mesh::initializeGlData(){
 	shader_program.initializeShader();
 	material.initializeMaterial();
@@ -38,14 +37,26 @@ void Mesh::initializeGlData(){
 
 void Mesh::bindMaterials(){
 	material.bind(); 
-
 }
 
 void Mesh::bindShaders(){
-	shader_program.bind(); 
+	if(!face_culling_enabled){ 
+		setFaceCulling(true); 
+		cullBackFace();
+		face_culling_enabled = true ; 
+	}
+	setDepthMask(true); 
+	setDepthFunc(LESS); 	
+	depth_mask_enabled = true ;
+	shader_program.bind(); 	
+	shader_program.setSceneCameraPointer(camera); 
+	if(camera->getType() == Camera::ARCBALL) 
+		model_matrix = camera->getSceneRotationMatrix() ;  
+	shader_program.setModelViewProjection(model_matrix) ; 
 }
 
 void Mesh::releaseShaders(){
+
 	shader_program.release(); 
 }
 void Mesh::clean(){
@@ -57,24 +68,111 @@ bool Mesh::isInitialized(){
 	return mesh_initialized; 	
 }
 
-
 void Mesh::setSceneCameraPointer(Camera *camera){
-	shader_program.setSceneCameraPointer(camera); 
+	this->camera = camera ;	
+}
+
+void Mesh::cullBackFace(){
+	glCullFace(GL_BACK); 
+}
+
+void Mesh::cullFrontFace(){
+	glCullFace(GL_FRONT); 
+}
+
+void Mesh::cullFrontAndBackFace(){
+	glCullFace(GL_FRONT_AND_BACK); 
+}	
+
+
+void Mesh::setFaceCulling(bool value){
+	if(value)
+		glEnable(GL_CULL_FACE); 
+	else
+		glDisable(GL_CULL_FACE);
+
+}
+
+void Mesh::setDepthMask(bool val){
+	glDepthMask(val ? GL_TRUE : GL_FALSE) ;
+	depth_mask_enabled = val ; 
+}
+
+void Mesh::setDepthFunc(DEPTHFUNC func){
+	glDepthFunc(func) ; 
+}
+/*********************************************************************************************/
+CubeMapMesh::CubeMapMesh() : Mesh() {
+	std::vector<float> vertices = { -1 , -1 , -1 ,  // 0
+					 1 , -1 , -1 ,  // 1
+					-1 , 1 , -1 ,   // 2
+				 	 1 , 1 , -1 ,   // 3
+					-1 , -1 , 1 ,   // 4
+					 1 , -1 , 1 ,   // 5
+					-1 , 1 , 1 ,    // 6
+				 	 1 , 1 , 1 };   // 7
+
+	std::vector<unsigned int> indices = { 0 , 1 , 2 , //Front face
+					      1 , 3 , 2 , //
+					      5 , 4 , 6 , //Back face
+					      6 , 7 , 5 , //
+				              0 , 2 , 6	, //Left face
+					      0 , 6 , 4	, //
+					      1 , 5 , 7	, //Right face
+					      7 , 3 , 1	, //
+					      3 , 7 , 6 , //Up face
+					      2 , 3 , 6 , //
+					      0 , 4 , 5 , //Down face
+					      0 , 5 , 1  //
+					      };
+	std::vector<float> textures = { 0 , 0 , 
+					1 , 0 , 
+					0 , 1 , 
+					1 , 1 , 
+					0 , 0 , 
+					1 , 0 , 
+					0 , 1 , 
+					1 , 1 };
+
+	std::vector<float> colors = { 1 , 0 , 0 ,
+				      1 , 0 , 0 , 	
+				      1 , 0 , 0 , 
+				      1 , 0 , 0 ,
+				      1 , 0 , 0 , 	
+				      1 , 0 , 0 , 
+				      1 , 0 , 0 , 
+				      1 , 0 , 0 };
+	geometry.indices = indices ; 
+	geometry.vertices = vertices ;
+	geometry.uv = textures ; 
+	geometry.colors = colors ; 
+	model_matrix = glm::mat4(1.f);
+	name="CubeMap" ;
 }
 
 
+CubeMapMesh::~CubeMapMesh(){
 
 
+}
 
-
-
-
-
-
-
-
-
-
+void CubeMapMesh::bindShaders(){
+	if(face_culling_enabled){
+		setFaceCulling(false);
+		face_culling_enabled = false;
+	}
+	setDepthFunc(LESS_OR_EQUAL); 	
+	shader_program.bind(); 	
+	shader_program.setSceneCameraPointer(camera); 	
+	glm::mat4 view = glm::mat4(glm::mat3(camera->getView()));
+	glm::mat4 projection = camera->getProjection() ; 
+	if(camera->getType() == Camera::ARCBALL){
+		model_matrix = camera->getSceneRotationMatrix() ;  
+		shader_program.setModelViewProjection(projection , view , model_matrix) ; 
+	}
+	else
+		shader_program.setModelViewProjection(projection , view , model_matrix) ; 
+}
 
 
 
