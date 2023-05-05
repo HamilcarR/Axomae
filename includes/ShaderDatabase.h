3808ab58cda00ace3d3bc8abbc7fe896e0ce6f51
Amine Bensalem <bensalem.amine321@gmail.com>
2023-05-05 21:53:49 +0100

    [FEATURE] adding shader database

--- includes/ShaderDatabase.h
+++ includes/ShaderDatabase.h
@@ -1,10 +1,33 @@
 #ifndef SHADERDATABASE_H
 #define SHADERDATABASE_H
 
+#include <map> 
+
+#include "utils_3D.h"
+#include "Shader.h"
+
 class ShaderDatabase{
 public:
+	static ShaderDatabase* getInstance(); 
+	void clean(); 
+	void destroy(); 
+	static bool isInstanced(){return instance != nullptr;}		
+	
+	/* compare the fragment / vertex / geom / tesselation code of shader to each instance in shader_database ... 
+	 * if an identical shader is found , return it's address . If not , store the new shader in the database , and returns it's address 
+	 */ 
+	Shader* addShader(const std::string vertex_code , const std::string fragment_code ,const Shader::TYPE type);  
+	bool contains(const Shader::TYPE type); 
+	Shader* get(Shader::TYPE type) const ; 
+private:
 	ShaderDatabase(); 
-	virtual ~ShaderDatabase() ; 
+	virtual ~ShaderDatabase() ;
+	
+
+private:
+	static ShaderDatabase* instance; 
+	std::map<int, Shader*> shader_database ; 
+
 
 }; 
 
--- sources/ShaderDatabase.cpp
+++ sources/ShaderDatabase.cpp
@@ -1,7 +1,53 @@
 #include "../includes/ShaderDatabase.h" 
+#include "../includes/ShaderFactory.h"
+
+ShaderDatabase* ShaderDatabase::instance = nullptr; 
+
+
+
+ShaderDatabase* ShaderDatabase::getInstance(){
+	if( instance == nullptr)
+		instance = new ShaderDatabase() ; 
+	return instance ; 
+}
+
+void ShaderDatabase::destroy(){
+	if(instance != nullptr)
+		delete instance ; 
+	instance = nullptr; 
+}
+
+void ShaderDatabase::clean(){
+	for(auto A : shader_database){
+		A.second->clean(); 
+		delete A.second ; 
+		A.second = nullptr ; 
+	} 
+	shader_database.clear() ; 
+}
+
+
+Shader*  ShaderDatabase::addShader(const std::string vertex_code ,const std::string fragment_code ,const Shader::TYPE type){
+	if(!contains(type))
+		shader_database[type] = ShaderFactory::constructShader(vertex_code , fragment_code , type) ; 
+	return shader_database[type] ; 
+}
+
+
+bool ShaderDatabase::contains(const Shader::TYPE type){
+	return shader_database.find(type) != shader_database.end(); 
+}
+
+Shader* ShaderDatabase::get(const Shader::TYPE type) const {
+	auto it = shader_database.find(type);
+	if(it != shader_database.end())
+		return it->second ;
+	else
+		return nullptr;
+}
+
 
 ShaderDatabase::ShaderDatabase(){
-
 
 }
 
