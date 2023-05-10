780fdb7f32e56989aad028b7fb374a2e887d9633
Amine Bensalem <bensalem.amine321@gmail.com>
2023-05-10 02:18:12 +0100

    [CODE] Moved uniforms strings to new file

--- includes/UniformNames.h
+++ includes/UniformNames.h
@@ -1,7 +1,18 @@
 #ifndef UNIFORMNAMES_H
 #define UNIFORMNAMES_H
 
+/*Shader matrix uniform names*/
+constexpr const char uniform_name_matrix_model[] = "MAT_MODEL"; 
+constexpr const char uniform_name_matrix_view[] = "MAT_VIEW" ;
+constexpr const char uniform_name_matrix_modelview[] = "MAT_MODELVIEW";  
+constexpr const char uniform_name_matrix_projection[] = "MAT_PROJECTION"; 
+constexpr const char uniform_name_matrix_view_projection[] = "MAT_VP"; 
+constexpr const char uniform_name_matrix_model_view_projection[] = "MAT_MVP" ; 
+constexpr const char uniform_name_matrix_normal[] = "MAT_NORMAL" ;
 
+
+/*Shader variables uniforms*/
+constexpr const char uniform_name_vector_camera_position[] = "camera_position" ; 
 
 
 #endif
