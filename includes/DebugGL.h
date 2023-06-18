b0e4bcad1238320d20692458fc75fe5322fdc9a1
Amine Bensalem <bensalem.amine321@gmail.com>
2023-06-18 18:03:45 +0100

    [FEATURE] Debug line + file informations

--- includes/DebugGL.h
+++ includes/DebugGL.h
@@ -3,7 +3,25 @@
 
 #include <iostream>
 #include "utils_3D.h" 
+#include <GL/glew.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
 
+
+inline void errorCheck(const char* filename , unsigned int line ){
+	GLenum error = GL_NO_ERROR;
+    error = glGetError();
+    if (error != GL_NO_ERROR) 
+        std::cerr << "GL error in file : " << filename << " at line : " << line << " with error : " << error << std::endl ; 
+
+}
+
+
+
+
+class GLDebugLogs{
+
+};
 
 
 #endif 
