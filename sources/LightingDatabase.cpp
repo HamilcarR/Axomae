35eb42d41639948543453c651015789349a9ca90
Amine Bensalem <bensalem.amine321@gmail.com>
2023-07-03 19:24:25 +0100

    [FIX] Fixed invalid iterator used while deleting elements in LightingDatabase::clearDatabase()

--- sources/LightingDatabase.cpp
+++ sources/LightingDatabase.cpp
@@ -4,17 +4,7 @@
 
 
 LightingDatabase::LightingDatabase(){ 
-    AbstractLight *L1 = new SpotLight(glm::vec3(-70 , 130 , 5) , glm::vec3(0.f) , glm::vec3(2.f , 1.8f , 0.8f), 25.f , 5.9f); 
-    AbstractLight *L2 = new PointLight(glm::vec3(-60 , 80 , -35) , glm::vec3(0.875f , 0.557f , 0.184f), glm::vec3(1.f , 0.0045 , 0.0075) , 50.f); 
-    AbstractLight *L3 = new PointLight(glm::vec3(100 , 0 , 0) , glm::vec3(1.f , 0.3f , 1.f), glm::vec3(1.f , 0.045 , 0.75) , 1.f); 
-    AbstractLight *L4 = new PointLight(glm::vec3(0 , 0 , 300) , glm::vec3(0.f , 1.f , 0.9f), glm::vec3(1.f , 0.045 , 0.0075) , 1.f);  
-   // AbstractLight *L5 = new DirectionalLight(glm::vec3(-100 , 0 , 0) , glm::vec3(1.0f , 1.f , 1.f) , 1.f); 
-    
-    addLight(L1); 
-    addLight(L2); 
-    addLight(L3); 
-    addLight(L4);
-    //addLight(L5);  
+   
 }
 
 LightingDatabase::~LightingDatabase(){
@@ -32,7 +22,7 @@ bool LightingDatabase::addLight(AbstractLight* light){
 const std::vector<AbstractLight*>& LightingDatabase::getLightsArrayByType(AbstractLight::TYPE type) {
     return light_database[type] ;
 }
-
+//TODO: [AX-36] fix invalidated iterator after erase
 void LightingDatabase::eraseLightsArray(AbstractLight::TYPE type){
     auto arr = light_database[type] ; 
     for(AbstractLight* A :  arr)
@@ -41,20 +31,24 @@ void LightingDatabase::eraseLightsArray(AbstractLight::TYPE type){
 }
 
 void LightingDatabase::clearDatabase(){
-    for(auto it = light_database.begin() ; it != light_database.end() ; it++)
-        eraseLightsArray(it->first); 
+    for(auto it = light_database.begin() ; it != light_database.end() ; it++){
+        auto type_array = light_database[it->first] ;
+        for(auto *A : type_array){
+            delete A ;     
+        }
+    }
     light_database.clear(); 
 }
 
-void LightingDatabase::updateShadersData(AbstractLight::TYPE type , Shader* shader , glm::mat4& modelview){
+void LightingDatabase::updateShadersData(AbstractLight::TYPE type , Shader* shader , glm::mat4& view){
     std::vector<AbstractLight*> array = light_database[type] ; 
     unsigned int index = 0 ;
     for(auto it = array.begin() ; it != array.end() ; it++ , index ++  )    
-        (*it)->updateShaderData(shader , modelview , index);    
+        (*it)->updateShaderData(shader , view , index);    
 }
 
 // First loop is to set up the max number of lights of each type. 
-void LightingDatabase::updateShadersData(Shader* shader , glm::mat4& modelview){
+void LightingDatabase::updateShadersData(Shader* shader , glm::mat4& view){
     for(auto it = light_database.begin() ; it != light_database.end() ; it++){
         unsigned int light_num = it->second.size(); 
         switch(it->first){
@@ -69,6 +63,6 @@ void LightingDatabase::updateShadersData(Shader* shader , glm::mat4& modelview){
             break;
 
         }
-        updateShadersData(it->first , shader , modelview); 
+        updateShadersData(it->first , shader , view); 
     }
 }
