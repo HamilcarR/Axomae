0a1679e58b6c0cf51e1ac0f9bf89e0675dd1c6fd
Amine Bensalem <bensalem.amine321@gmail.com>
2023-07-10 06:16:03 +0100

    [UNIT-TEST] Added minmax compute test

--- tests/BoundingBoxTest.cpp
+++ tests/BoundingBoxTest.cpp
@@ -1,9 +1,10 @@
 #include "Test.h"
 #include "../includes/BoundingBox.h"
 
-#define m_rand rand()%100 + 1 
+#define m_rand rand()%100 - 50 
+#define f_rand rand() / ((float) RAND_MAX) * 100 - 50
 
-
+constexpr float EPSILON = 0.0001f ; 
 constexpr unsigned ITERATION_NUMBER = 5; 
 std::vector<float> vertices = {
 		-1 , -1 , -1 	,  // 0
@@ -20,6 +21,32 @@ std::vector<float> vertices = {
 
 
 
+TEST(BoundingBoxTest , minMaxCompute){
+    std::vector<float> v;
+    srand(time(nullptr));  
+    glm::vec3 max_coords = glm::vec3(-INT_MAX , -INT_MAX , -INT_MAX);  
+    glm::vec3 min_coords = glm::vec3(-1.f) * max_coords; 
+    for(unsigned i = 0 ; i < 24 ; i++) 
+        v.push_back(f_rand); 
+    for(unsigned i = 0 ; i < 24 ; i+=3){
+        max_coords.x = v[i] >= max_coords.x ? v[i] : max_coords.x; 
+        max_coords.y = v[i+1] >= max_coords.y ? v[i+1] : max_coords.y; 
+        max_coords.z = v[i+2] >= max_coords.z ? v[i+2] : max_coords.z; 
+        min_coords.x = v[i] < min_coords.x ? v[i] : min_coords.x; 
+        min_coords.y = v[i+1] < min_coords.y ? v[i+1] : min_coords.y; 
+        min_coords.z = v[i+2] < min_coords.z ? v[i+2] : min_coords.z; 
+
+    }
+    BoundingBox B1(v);
+    auto B1_max = B1.getMaxCoords(); 
+    auto B1_min = B1.getMinCoords(); 
+    EXPECT_EQ(B1_max , max_coords); 
+    EXPECT_EQ(B1_min , min_coords); 
+
+}
+
+
+
 
 TEST(BoundingBoxTest , productOperatorTest){
     BoundingBox B1(vertices); 
@@ -27,10 +54,10 @@ TEST(BoundingBoxTest , productOperatorTest){
     std::vector<std::pair<glm::mat4 , glm::vec3[3]>> matrix_result ; //first = matrices , second = result matrix x coords => {min_coords , max_coords , center}   
     matrix_result.resize(ITERATION_NUMBER); 
     for(unsigned i = 0 ; i < ITERATION_NUMBER; i++){
-        glm::vec4 row1(m_rand , m_rand , m_rand , m_rand);
-        glm::vec4 row2(m_rand , m_rand , m_rand , m_rand);
-        glm::vec4 row3(m_rand , m_rand , m_rand , m_rand);
-        glm::vec4 row4(m_rand , m_rand , m_rand , m_rand); 
+        glm::vec4 row1(f_rand , f_rand , f_rand , f_rand);
+        glm::vec4 row2(f_rand , f_rand , f_rand , f_rand);
+        glm::vec4 row3(f_rand , f_rand , f_rand , f_rand);
+        glm::vec4 row4(f_rand , f_rand , f_rand , f_rand); 
         glm::mat4 mat(row1,row2,row3,row4);
         glm::vec3 min_coords = mat * glm::vec4(B1.getMinCoords() , 1.f); 
         glm::vec3 max_coords = mat * glm::vec4(B1.getMaxCoords() , 1.f); 
@@ -45,9 +72,15 @@ TEST(BoundingBoxTest , productOperatorTest){
         glm::vec3 min_coords_B1 = matrix_result[i].second[0];
         glm::vec3 max_coords_B1 = matrix_result[i].second[1]; 
         glm::vec3 center_B1 = matrix_result[i].second[2];
-        EXPECT_EQ(B2.getMinCoords() , min_coords_B1); 
-        EXPECT_EQ(B2.getMaxCoords() , max_coords_B1); 
-        EXPECT_EQ(B2.getPosition() , center_B1); 
+        EXPECT_LE(glm::abs(B2.getMinCoords().x - min_coords_B1.x) , EPSILON); 
+        EXPECT_LE(glm::abs(B2.getMinCoords().y - min_coords_B1.y) , EPSILON); 
+        EXPECT_LE(glm::abs(B2.getMinCoords().z - min_coords_B1.z) , EPSILON); 
+        EXPECT_LE(glm::abs(B2.getMaxCoords().x - max_coords_B1.x) , EPSILON); 
+        EXPECT_LE(glm::abs(B2.getMaxCoords().y - max_coords_B1.y) , EPSILON); 
+        EXPECT_LE(glm::abs(B2.getMaxCoords().z - max_coords_B1.z) , EPSILON); 
+        EXPECT_LE(glm::abs(B2.getPosition().x - center_B1.x) , EPSILON); 
+        EXPECT_LE(glm::abs(B2.getPosition().y - center_B1.y) , EPSILON); 
+        EXPECT_LE(glm::abs(B2.getPosition().z - center_B1.z) , EPSILON); 
     }
 
 
