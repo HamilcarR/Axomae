00ef8baca39262367acda6c460c75f2b1e8bb146
Amine Bensalem <bensalem.amine321@gmail.com>
2023-08-23 02:06:03 +0100

    [CODE-DESIGN] Moved low discrepancy rand generators to cuh file

--- kernels/CubemapProcessing.cuh
+++ kernels/CubemapProcessing.cuh
@@ -58,8 +58,41 @@ inline const glm::dvec2 cartesianToSpherical(const glm::dvec3 xyz){
     return cartesianToSpherical(xyz.x , xyz.y , xyz.z); 
 }
 
+__host__ __device__
+inline glm::vec3 pgc3d(unsigned x , unsigned y , unsigned z) {
+    x = x * 1664525u + 1013904223u; 
+    y = y * 1664525u + 1013904223u;
+    z = z * 1664525u + 1013904223u;
 
+    x += y*z; 
+    y += z*x; 
+    z += x*y;
+    
+    x ^= x >> 16u;
+    y ^= y >> 16u;
+    z ^= z >> 16u;
+    
+    x += y*z; 
+    y += z*x; 
+    z += x*y;
 
+    return glm::vec3(x , y , z) * (1.f / float(0xFFFFFFFFu));
+}
+
+__host__ __device__
+inline double radicalInverse(unsigned bits) {
+    bits = (bits << 16u) | (bits >> 16u);
+    bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
+    bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
+    bits = ((bits & 0x0F0F0F0Fu) << 4u) | ((bits & 0xF0F0F0F0u) >> 4u);
+    bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
+    return double(bits) * 2.3283064365386963e-10; 
+}
+
+__host__ __device__
+inline glm::dvec3 hammersley3D(unsigned  i , unsigned N){
+    return glm::dvec3(double(i) / double(N) , radicalInverse(i) , radicalInverse(i ^ 0xAAAAAAAAu)); 
+}
 
 
 
