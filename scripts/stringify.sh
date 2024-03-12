f122297b89b7489be128c7d7e2b81168f45f0693
Amine Bensalem <bensalem.amine321@gmail.com>
2024-03-12 18:32:53 +0100

    glsl generation script fixed

--- scripts/stringify.sh
+++ scripts/stringify.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
+
+# refactor or integrate fully to cmake
 PROJECT_DIR=$1
-echo $PROJECT_DIR
-cd $PROJECT_DIR/sources/gpu/opengl/shader
+shader_path=$PROJECT_DIR/sources/gpu/opengl/shader
+echo "Moving to $shader_path" 
+cd $shader_path 
 
 echo "Executing stringify.sh ..."
 echo "Generating vertex shaders strings"
 for f in glsl/*.vert; do
-    xxd -i glsl/"${f##*/}" >>utils/temp_vertex.h
+    xxd -i glsl/"${f##*/}" >> utils/temp_vertex.h
 done
 
 echo "Generating fragment shaders strings"
 for f in glsl/*.frag; do
-    xxd -i glsl/"${f##*/}" >>utils/temp_fragment.h
+    echo $f;
+    xxd -i glsl/"${f##*/}" >> utils/temp_fragment.h
 done
+
+
 
 if [ -f utils/vertex.h ]; then
     rm utils/vertex.h
@@ -22,6 +28,8 @@ fi
 if [ -f utils/fragment.h ]; then
     rm utils/fragment.h
 fi
+
 mv utils/temp_vertex.h utils/vertex.h
 mv utils/temp_fragment.h utils/fragment.h
+
 echo "Shaders generation done..."
