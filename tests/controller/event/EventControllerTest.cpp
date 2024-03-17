936d97b22f1471eba059530b83337c50d87ae09c
Amine Bensalem <bensalem.amine321@gmail.com>
2024-03-17 02:48:24 +0100

    Unit test for the event controller

---
+++ tests/controller/event/EventControllerTest.cpp
@@ -0,0 +1,9 @@
+#include "EventControllerTest.h"
+#include "EventController.h"
+#include "Test.h"
+// Tests in case we extend the event manager
+class EventEmitter {};
+class EventControllerTest {
+ public:
+  controller::event::Event event;
+};
