26065254bda7c7f2967a1e683878874067f8296a
Amine Bensalem <bensalem.amine321@gmail.com>
2024-03-19 12:37:50 +0100

    Format debug module

--- sources/debug/PerformanceLogger.h
+++ sources/debug/PerformanceLogger.h
@@ -9,32 +9,25 @@
  */
 
 class PerformanceLogger : protected AbstractLogger {
- public:
-  PerformanceLogger() = default;
-
-  void startTimer() {
-    std::cout << "Timer started!\n";
-    start = std::chrono::high_resolution_clock::now();
-  }
-
-  void endTimer() {
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "Timer end!\n";
-  }
-
-  /**
-   * @brief Print the difference between time at end , and time at start
-   *
-   */
-  void print() const override {
-    std::chrono::milliseconds dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Duration : " << dur.count() << "ms\n";
-  }
-
  protected:
   std::chrono::high_resolution_clock::time_point start{};
   std::chrono::high_resolution_clock::time_point end{};
   std::chrono::milliseconds duration{};
+
+ public:
+  PerformanceLogger() = default;
+  void startTimer() {
+    std::cout << "Timer started!\n";
+    start = std::chrono::high_resolution_clock::now();
+  }
+  void endTimer() {
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << "Timer end!\n";
+  }
+  void print() const override {
+    std::chrono::milliseconds dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Duration : " << dur.count() << "ms\n";
+  }
 };
 
 #endif
