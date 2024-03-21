70b808143b52a14679cee120f28bba27d906330a
Amine Bensalem <bensalem.amine321@gmail.com>
2024-03-21 03:33:51 +0100

    Cleanup

--- sources/common/math/math_utils.h
+++ sources/common/math/math_utils.h
@@ -44,41 +44,41 @@ namespace math {
 
   namespace spherical {
     template<class T>
-    inline const glm::dvec2 uvToSpherical(const T &u, const T &v) {
+    inline glm::dvec2 uvToSpherical(const T &u, const T &v) {
       const T phi = 2 * PI * u;
       const T theta = PI * v;
       return glm::dvec2(phi, theta);
     }
 
-    inline const glm::dvec2 uvToSpherical(const glm::dvec2 &uv) { return uvToSpherical(uv.x, uv.y); }
+    inline glm::dvec2 uvToSpherical(const glm::dvec2 &uv) { return uvToSpherical(uv.x, uv.y); }
 
     template<class T>
-    inline const glm::dvec2 sphericalToUv(const T &phi, const T &theta) {
+    inline glm::dvec2 sphericalToUv(const T &phi, const T &theta) {
       const T u = phi / (2 * PI);
       const T v = theta / PI;
       return glm::dvec2(u, v);
     }
 
-    inline const glm::dvec2 sphericalToUv(const glm::dvec2 &sph) { return sphericalToUv(sph.x, sph.y); }
+    inline glm::dvec2 sphericalToUv(const glm::dvec2 &sph) { return sphericalToUv(sph.x, sph.y); }
 
     template<class T>
-    inline const glm::dvec3 sphericalToCartesian(const T &phi, const T &theta) {
+    inline glm::dvec3 sphericalToCartesian(const T &phi, const T &theta) {
       const T z = cos(theta);
       const T x = sin(theta) * cos(phi);
       const T y = sin(theta) * sin(phi);
       return glm::dvec3(x, y, z);
     }
 
-    inline const glm::dvec3 sphericalToCartesian(const glm::dvec2 &sph) { return sphericalToCartesian(sph.x, sph.y); }
+    inline glm::dvec3 sphericalToCartesian(const glm::dvec2 &sph) { return sphericalToCartesian(sph.x, sph.y); }
 
     template<class T>
-    inline const glm::dvec2 cartesianToSpherical(const T &x, const T &y, const T &z) {
+    inline glm::dvec2 cartesianToSpherical(const T &x, const T &y, const T &z) {
       const T theta = acos(z);
       const T phi = atan2f(y, x);
       return glm::dvec2(phi, theta);
     }
 
-    inline const glm::dvec2 cartesianToSpherical(const glm::dvec3 &xyz) { return cartesianToSpherical(xyz.x, xyz.y, xyz.z); }
+    inline glm::dvec2 cartesianToSpherical(const glm::dvec3 &xyz) { return cartesianToSpherical(xyz.x, xyz.y, xyz.z); }
 
   }  // namespace spherical
 
