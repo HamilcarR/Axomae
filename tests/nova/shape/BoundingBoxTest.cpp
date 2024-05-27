8fff61c77a46412f799086769b61f56b2f0d56e5
Amine Bensalem <bensalem.amine321@gmail.com>
2024-05-27 01:49:17 +0100

    Intersection Unit tests

--- tests/nova/shape/BoundingBoxTest.cpp
+++ tests/nova/shape/BoundingBoxTest.cpp
@@ -2,11 +2,14 @@
 #include "Test.h"
 #include "math_utils.h"
 #include "ray/Ray.h"
-#define f_rand math::random::nrandf(-10.040, 12.100)
+#define f_rand math::random::nrandf(-2000.f, 2000.f)
 using namespace nova::shape;
 
-constexpr float EPSILON = 0.0001f;
-constexpr unsigned ITERATION_NUMBER = 5;
+constexpr float EPSILON = 0.001f;
+constexpr unsigned ITERATION_NUMBER = 50;
+
+const float MIN_COORD = -200.f;
+const float MAX_COORD = 200.f;
 const std::vector<float> vertices = {
     -1, -1, -1,  // 0
     1,  -1, -1,  // 1
@@ -16,6 +19,40 @@ const std::vector<float> vertices = {
     1,  -1, 1,   // 5
     -1, 1,  1,   // 6
     1,  1,  1    // 7
+};
+
+// clang-format off
+const std::vector<float> directions = {
+  1.f, 0.f , 0.f ,
+  0.f , 1.f , 0.f ,
+  0.f , 0.f , 1.f ,
+  -1.f , 0.f , 0.f ,
+  0.f , -1.f , 0.f ,
+  0.f , 0.f , -1.f
+
+};
+
+// clang-format on
+
+class AABBGenerator {
+ public:
+  std::array<float, 24> vertices;
+
+ public:
+  AABBGenerator() {
+    // clang-format off
+    vertices = {
+        -MAX_COORD, -MAX_COORD, -MAX_COORD,
+        MAX_COORD,  -MAX_COORD, -MAX_COORD,
+        -MAX_COORD, MAX_COORD,  -MAX_COORD,
+        MAX_COORD,  MAX_COORD,  -MAX_COORD,
+        -MAX_COORD, -MAX_COORD, MAX_COORD,
+        MAX_COORD,  -MAX_COORD, MAX_COORD,
+        -MAX_COORD, MAX_COORD,  MAX_COORD,
+        MAX_COORD,  MAX_COORD,  MAX_COORD
+    };
+    // clang-format on
+  }
 };
 
 TEST(BoundingBoxTest, minMaxCompute) {
@@ -75,4 +112,50 @@ TEST(BoundingBoxTest, productOperatorTest) {
   }
 }
 
-TEST(BoundingBoxTest, intersect) { BoundingBox B1(vertices); }
+inline bool is_outside_box(const glm::vec3 &position, const BoundingBox &test) {
+  return position.x < test.getMinCoords().x || position.x > test.getMaxCoords().x || position.y < test.getMinCoords().y ||
+         position.y > test.getMaxCoords().y || position.z < test.getMinCoords().z || position.z > test.getMaxCoords().z;
+}
+
+TEST(BoundingBoxTest, outIntersection) {
+  AABBGenerator vert_gen;
+  std::vector<float> vector_vertices;
+  vector_vertices.resize(vert_gen.vertices.size());
+  std::copy(vert_gen.vertices.begin(), vert_gen.vertices.end(), vector_vertices.begin());
+  BoundingBox B1(vector_vertices);
+  for (unsigned i = 0; i < ITERATION_NUMBER; i++) {
+    glm::vec3 out_position;
+    bool isoutside = false;
+    do {
+      out_position = {f_rand, f_rand, f_rand};
+      isoutside = is_outside_box(out_position, B1);
+    } while (!isoutside);
+    const nova::Ray ray(out_position, glm::normalize(B1.getPosition() - out_position));
+    glm::vec3 n;
+    float intersect_dist = 0.f;
+    EXPECT_TRUE(B1.intersect(ray, 0.001, 10000.f, n, intersect_dist));
+  }
+}
+
+TEST(BoundingBoxTest, inIntersection) {
+  AABBGenerator vert_gen;
+  std::vector<float> vector_vertices;
+  vector_vertices.resize(vert_gen.vertices.size());
+  std::copy(vert_gen.vertices.begin(), vert_gen.vertices.end(), vector_vertices.begin());
+  BoundingBox B1(vector_vertices);
+  for (unsigned i = 0; i < ITERATION_NUMBER; i++) {
+    glm::vec3 out_position;
+    bool isoutside = false;
+    do {
+      out_position = {f_rand, f_rand, f_rand};
+      isoutside = is_outside_box(out_position, B1);
+    } while (isoutside);
+    for (int i = 0; i < directions.size(); i += 3) {
+      const glm::vec3 direction{directions[i], directions[i + 1], directions[i + 2]};
+      const nova::Ray ray(out_position, direction);
+      glm::vec3 n;
+      float intersect_dist = 0.f;
+      EXPECT_TRUE(B1.intersect(ray, 0.001, 10000.f, n, intersect_dist));
+    }
+  }
+}
