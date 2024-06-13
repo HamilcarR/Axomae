086726c2c326982b5657af8c482a1a4761d48996
Amine Bensalem <bensalem.amine321@gmail.com>
2024-06-13 10:19:46 +0100

    Cleanup

---
+++ sources/renderer/NovaRenderer_postprocess.cpp
@@ -0,0 +1,75 @@
+#include "Camera.h"
+#include "CameraFrameBuffer.h"
+#include "NovaRenderer.h"
+#include "RenderPipeline.h"
+#include "Scene.h"
+void NovaRenderer::setGammaValue(float value) {
+  if (camera_framebuffer) {
+    camera_framebuffer->setGamma(value);
+  }
+}
+
+void NovaRenderer::setExposureValue(float value) {
+  if (camera_framebuffer) {
+    camera_framebuffer->setExposure(value);
+  }
+}
+
+void NovaRenderer::setNoPostProcess() {
+  if (camera_framebuffer) {
+    camera_framebuffer->setPostProcessDefault();
+  }
+}
+
+void NovaRenderer::setPostProcessEdge() {
+  if (camera_framebuffer) {
+    camera_framebuffer->setPostProcessEdge();
+  }
+}
+
+void NovaRenderer::setPostProcessSharpen() {
+  if (camera_framebuffer) {
+    camera_framebuffer->setPostProcessSharpen();
+  }
+}
+
+void NovaRenderer::setPostProcessBlurr() {
+  if (camera_framebuffer) {
+    camera_framebuffer->setPostProcessBlurr();
+  }
+}
+
+void NovaRenderer::resetSceneCamera() {
+  if (scene_camera) {
+    scene_camera->reset();
+  }
+}
+
+void NovaRenderer::setRasterizerFill() {
+  if (scene) {
+    scene->setPolygonFill();
+  }
+}
+
+void NovaRenderer::setRasterizerPoint() {
+  if (scene) {
+    scene->setPolygonPoint();
+  }
+}
+
+void NovaRenderer::setRasterizerWireframe() {
+  if (scene) {
+    scene->setPolygonWireframe();
+  }
+}
+
+void NovaRenderer::displayBoundingBoxes(bool display) {
+  if (scene) {
+    scene->displayBoundingBoxes(display);
+  }
+}
+void NovaRenderer::setViewerWidget(GLViewer *widget) {
+  gl_widget = widget;
+  if (render_pipeline)
+    render_pipeline->setContextSwitcher(widget);
+}
