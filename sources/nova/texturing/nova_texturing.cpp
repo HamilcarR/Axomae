af081a43bdac9c8b664ac6c1b6dd34d8ba132012
Amine Bensalem <bensalem.amine321@gmail.com>
2024-07-10 14:40:37 +0100

    Refactor: Uncoupled envmap sampling in Nova from the texture module

--- sources/nova/texturing/nova_texturing.cpp
+++ sources/nova/texturing/nova_texturing.cpp
@@ -1,16 +1,21 @@
 #include "nova_texturing.h"
-#include "TextureProcessing.h"
 namespace nova::texturing {
   glm::vec3 sample_cubemap(const glm::vec3 &sample_vector, const TextureRawData *res_holder) {
     AX_ASSERT(res_holder != nullptr, "");
-    const TextureOperations<float> texture_processor(*res_holder->raw_data, res_holder->width, res_holder->height);
     glm::vec3 normalized = glm::normalize(sample_vector);
     std::swap(normalized.y, normalized.z);
     normalized.z = -normalized.z;
     const glm::vec2 sph = math::spherical::cartesianToSpherical(normalized);
     const glm::vec2 uv = math::spherical::sphericalToUv(sph);
+    const float u = uv.x;
+    const float v = uv.y;
 
-    return texture_processor.uvSample(uv.x, 1 - uv.y);
+    const int x = math::texture::uvToPixel(u, res_holder->width - 1);
+    const int y = math::texture::uvToPixel(1 - v, res_holder->height - 1);
+
+    int index = (y * res_holder->width + x) * res_holder->channels;
+    const float *data = res_holder->raw_data->data();
+    return {data[index], data[index + 1], data[index + 2]};
   }
 
   glm::vec3 sample_cubemap_plane(const glm::vec3 &sample_vector, const glm::vec3 &up_vector, const TextureRawData *res_holder) {
