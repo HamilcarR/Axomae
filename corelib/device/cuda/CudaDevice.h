e728f89fe536cf487b89fa8510c594424e77db6c
Amine Bensalem <bensalem.amine321@gmail.com>
2024-08-10 07:13:12 +0100

    Renamed cuda methods for better clarity , and better future polymorphism with hip method names

--- corelib/device/cuda/CudaDevice.h
+++ corelib/device/cuda/CudaDevice.h
@@ -17,27 +17,27 @@ namespace ax_cuda {
 
     /* Init */
     AX_HOST_ONLY explicit CudaDevice(int device);
-    AX_HOST_ONLY DeviceError init(const CudaParams &params);
-    AX_HOST_ONLY DeviceError set(const CudaParams &params);
+    AX_HOST_ONLY DeviceError GPUInitDevice(const CudaParams &params);
+    AX_HOST_ONLY DeviceError GPUSetDevice(const CudaParams &params);
 
     /* Memory */
-    AX_HOST_ONLY DeviceError allocateMemory(void **ptr, std::size_t size);
-    AX_HOST_ONLY DeviceError deallocateMemory(void *ptr);
-    AX_HOST_ONLY DeviceError allocateMemoryManaged(void **ptr, std::size_t, const CudaParams &params);
-    AX_HOST_ONLY DeviceError copyMemory(const void *ptr_source, void *ptr_dest, std::size_t byte_count, const CudaParams &params);
-    AX_HOST_ONLY DeviceError allocateMemoryArray(cudaArray_t *array, const CudaParams &params, unsigned width, unsigned height, unsigned flags = 0);
-    AX_HOST_ONLY DeviceError deallocateMemoryArray(cudaArray_t array);
-    AX_HOST_ONLY DeviceError copy2DToArray(
+    AX_HOST_ONLY DeviceError GPUMalloc(void **ptr, std::size_t size);
+    AX_HOST_ONLY DeviceError GPUFree(void *ptr);
+    AX_HOST_ONLY DeviceError GPUMallocManaged(void **ptr, std::size_t, const CudaParams &params);
+    AX_HOST_ONLY DeviceError GPUMemcpy(const void *ptr_source, void *ptr_dest, std::size_t byte_count, const CudaParams &params);
+    AX_HOST_ONLY DeviceError GPUMallocArray(cudaArray_t *array, const CudaParams &params, unsigned width, unsigned height, unsigned flags = 0);
+    AX_HOST_ONLY DeviceError GPUFreeArray(cudaArray_t array);
+    AX_HOST_ONLY DeviceError GPUMemcpy2DToArray(
         cudaArray_t array, size_t wOffset, size_t hOffset, const void *src, size_t spitch, size_t width, size_t height, const CudaParams &params);
-    AX_HOST_ONLY DeviceError registerMemoryHost(void *ptr, std::size_t size_bytes, unsigned flags);
-    AX_HOST_ONLY DeviceError getHostDevicePtr(void **ptr_device, void *ptr_host, unsigned flags);
-    AX_HOST_ONLY DeviceError unregisterMemoryHost(void *ptr_host);
+    AX_HOST_ONLY DeviceError GPUHostRegister(void *ptr, std::size_t size_bytes, unsigned flags);
+    AX_HOST_ONLY DeviceError GPUHostGetDevicePointer(void **ptr_device, void *ptr_host, unsigned flags);
+    AX_HOST_ONLY DeviceError GPUHostUnregister(void *ptr_host);
     /* Textures */
-    AX_HOST_ONLY DeviceError createTextureObject(cudaTextureObject_t *tex, const CudaParams &params, bool use_resc_view = false);
-    AX_HOST_ONLY DeviceError destroyTextureObject(cudaTextureObject_t texture_object);
+    AX_HOST_ONLY DeviceError GPUCreateTextureObject(cudaTextureObject_t *tex, const CudaParams &params, bool use_resc_view = false);
+    AX_HOST_ONLY DeviceError GPUDestroyTextureObject(cudaTextureObject_t texture_object);
 
     /* Synchronizations*/
-    AX_HOST_ONLY DeviceError synchronize();
+    AX_HOST_ONLY DeviceError GPUDeviceSynchronize();
   };
 
 }  // namespace ax_cuda
