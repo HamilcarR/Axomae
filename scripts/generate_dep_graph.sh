c79a126247bf028b0dcd41f2e03a1920adc3eab9
Amine Bensalem <bensalem.amine321@gmail.com>
2024-08-15 14:27:09 +0100

    Removed sxiv.

--- scripts/generate_dep_graph.sh
+++ scripts/generate_dep_graph.sh
@@ -4,4 +4,4 @@
 mkdir -p ../build_metadata/dependencies 
 cd ../build_metadata/dependencies
 cmake --graphviz=dependencies.dot ../../Axomae 
-dot -Tpng dependencies.dot -o dependencies.png && sxiv dependencies.png
+dot -Tpng dependencies.dot -o dependencies.png
