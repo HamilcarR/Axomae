0481909570259e92b4f65abe40b67653305bcec1
bensalem amine <bensalem.amine321@gmail.com>
2024-09-16 20:09:33 +0100

    Build: Added from source QT build
    - Allows to build QT from the same toolchain than the executable, and not rely on the system libraries.
    
     - This fixes incompatible linking symbols in case the user needs to compile with older versions of his compiler to access CUDA features.

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-
-
 ROOT="$(pwd)"
-echo 'Downloading dependencies ...'
+echo 'Downloading dependencies ...' 
+git submodule update --init --remote
 
-# Initialize dependencies that also need their submodules downloaded as well
-for dependency in assimp boost glm googletest imath libdeflate openexr SDL SDL_image stb zlib ; do
-  git submodule update --init --recursive vendor/$dependency
-done
-
-git submodule update --init vendor/qt
-echo 'Dependencies downloaded ...'
 if [ -d "$ROOT/vendor/qt" ]; then
 	cd $ROOT/vendor/qt || exit
 else
 	echo 'Error: The QT directory has not been created by submodule update ...'
 fi
+echo 'Dependencies downloaded ...' 
 
-
-echo 'Setting up required QT submodules ...'
-
+echo 'Setting up required QT submodules ...' 
 ./init-repository.pl --force --module-subset=qtbase
