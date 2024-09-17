9189803b682972a56168fe8fa0da2a9564e95152
bensalem amine <bensalem.amine321@gmail.com>
2024-09-17 08:26:47 +0100

    Build: Put back QT build to default on CI pipeline

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -3,13 +3,13 @@
 ROOT="$(pwd)"
 echo 'Downloading dependencies ...' 
 git submodule update --init --force
-
+echo 'Dependencies downloaded ...' 
 if [ -d "$ROOT/vendor/qt" ]; then
 	cd $ROOT/vendor/qt || exit
 else
 	echo 'Error: The QT directory has not been created by submodule update ...'
 fi
-echo 'Dependencies downloaded ...' 
+
 
 echo 'Setting up required QT submodules ...' 
 ./init-repository.pl --force --module-subset=qtbase
