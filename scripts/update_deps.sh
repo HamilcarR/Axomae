03a58d02555d52a77f219fe2566a11affa75fe11
bensalem amine <bensalem.amine321@gmail.com>
2024-09-17 21:19:42 +0100

    Build: Fixed dependency script not updating boost library
    The dependency script now downloads recursively every submodules except for QT

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+
+
 ROOT="$(pwd)"
-echo 'Downloading dependencies ...' 
-git submodule update --init --force
+echo 'Downloading dependencies ...'
+
+# Initialize dependencies that also need their submodules downloaded as well
+for dependency in assimp boost glm googletest imath libdeflate openexr SDL SDL_image stb zlib ; do
+  git submodule update --init --recursive vendor/$dependency
+done
 echo 'Dependencies downloaded ...' 
 if [ -d "$ROOT/vendor/qt" ]; then
 	cd $ROOT/vendor/qt || exit
