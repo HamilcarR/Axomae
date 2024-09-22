b663a091b02b46a3a2acaf3904fefdff766d6506
bensalem amine <bensalem.amine321@gmail.com>
2024-09-22 12:10:26 +0100

    Feature: Added emissive texture integrator for nova
    Refactor: Replaced error_id by a uin64_t for bitwise operations

--- corelib/nova/gpu/gpu.cuh
+++ corelib/nova/gpu/gpu.cuh
@@ -4,11 +4,6 @@
 
 namespace nova {
 
-  class NovaResourceManager;
-  namespace gpu {
-    AX_KERNEL void test_func(float *ptr, unsigned width, unsigned height, nova::nova_eng_internals &nova_internals);
-  }
-
   void launch_gpu_kernel(HdrBufferStruct *buffers,
                          unsigned width_resolution,
                          unsigned height_resolution,
