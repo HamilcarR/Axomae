38261f119a44004ac86115ee7869c74522682cdf
bensalem amine <bensalem.amine321@gmail.com>
2024-09-22 01:31:25 +0100

    Fix: Added a synchronized error flag in NovaException class
    Considering that NovaException is a class that is also executed on cuda, and therefore needs cuda's version of atomics, added synchronized_err_flag property as a way to transfer errors stored in the atomic err_flag.

--- corelib/nova/manager/NovaExceptionManager.h
+++ corelib/nova/manager/NovaExceptionManager.h
@@ -8,10 +8,16 @@ namespace nova {
     nova::exception::NovaException exception;
 
    public:
-    CLASS_CM(NovaExceptionManager)
+    AX_DEVICE_CALLABLE NovaExceptionManager() = default;
+    AX_DEVICE_CALLABLE ~NovaExceptionManager() = default;
+    AX_DEVICE_CALLABLE NovaExceptionManager(const NovaExceptionManager &copy) = default;
+    AX_DEVICE_CALLABLE NovaExceptionManager(NovaExceptionManager &&move) noexcept = default;
+    AX_DEVICE_CALLABLE NovaExceptionManager &operator=(const NovaExceptionManager &copy) = default;
+    AX_DEVICE_CALLABLE NovaExceptionManager &operator=(NovaExceptionManager &&move) noexcept = default;
 
     GENERATE_GETTERS(exception::NovaException, ExceptionReference, exception)
-    AX_DEVICE_CALLABLE uint64_t checkErrorStatus() const { return exception.errorCheck(); }
+
+    AX_DEVICE_CALLABLE [[nodiscard]] uint64_t checkErrorStatus() const { return exception.getErrorFlag(); }
     AX_DEVICE_CALLABLE void addError(nova::exception::ERROR error_id) { exception.addErrorType(error_id); }
     AX_DEVICE_CALLABLE void addError(const nova::exception::NovaException &other_exception);
     [[nodiscard]] std::vector<nova::exception::ERROR> getErrorList() const { return exception.getErrorList(); }
