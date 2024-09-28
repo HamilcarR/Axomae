f22dfe07462e72842e12d264033bfb61c4553811
bensalem amine <bensalem.amine321@gmail.com>
2024-09-28 05:04:26 +0100

    WIP: Start implementing mega-kernel
    - Moved the feature to the integrator module, will now build conditionally depending on AXOMAE_USE_CUDA

--- corelib/internal/common/math/math_texturing.h
+++ corelib/internal/common/math/math_texturing.h
@@ -1,20 +1,21 @@
 #ifndef MATH_TEXTURING_H
 #define MATH_TEXTURING_H
+#include "device_utils.h"
 namespace math::texture {
   constexpr float ONE_255 = 1.f / 255.f;
 
   template<class T>
-  inline float rgb_uint2float(T rgb) {
+  AX_DEVICE_CALLABLE float rgb_uint2float(T rgb) {
     return (float)rgb * ONE_255;
   }
 
   template<class D>
-  double pixelToUv(D coord, const unsigned dim) {
+  AX_DEVICE_CALLABLE double pixelToUv(D coord, const unsigned dim) {
     return static_cast<double>(coord) / static_cast<double>(dim);
   }
 
   template<class D>
-  unsigned uvToPixel(D coord, unsigned dim) {
+  AX_DEVICE_CALLABLE unsigned uvToPixel(D coord, unsigned dim) {
     return static_cast<unsigned>(coord * dim);
   }
 }  // namespace math::texture
