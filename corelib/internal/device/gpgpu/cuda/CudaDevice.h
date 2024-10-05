29dcf4bf94d349e2beb2d94c40034821f4e8bb3a
bensalem amine <bensalem.amine321@gmail.com>
2024-10-05 14:19:35 +0100

    Refactor: Simpler error checking cuda

--- corelib/internal/device/gpgpu/cuda/CudaDevice.h
+++ corelib/internal/device/gpgpu/cuda/CudaDevice.h
@@ -1,6 +1,5 @@
 #ifndef CUDADEVICE_H
 #define CUDADEVICE_H
-#include "../DeviceError.h"
 #include "../device_utils.h"
 #include "CudaParams.h"
 #include "cuda_utils.h"
@@ -18,27 +17,27 @@ namespace ax_cuda {
 
     /* Init */
     AX_HOST_ONLY explicit CudaDevice(int device);
-    AX_HOST_ONLY DeviceError GPUInitDevice(const CudaParams &params);
-    AX_HOST_ONLY DeviceError GPUSetDevice(const CudaParams &params);
+    AX_HOST_ONLY void GPUInitDevice(const CudaParams &params);
+    AX_HOST_ONLY void GPUSetDevice(const CudaParams &params);
 
     /* Memory */
-    AX_DEVICE_CALLABLE DeviceError GPUMalloc(void **ptr, std::size_t size);
-    AX_DEVICE_CALLABLE DeviceError GPUFree(void *ptr);
-    AX_HOST_ONLY DeviceError GPUMallocManaged(void **ptr, std::size_t, const CudaParams &params);
-    AX_HOST_ONLY DeviceError GPUMemcpy(const void *ptr_source, void *ptr_dest, std::size_t byte_count, const CudaParams &params);
-    AX_HOST_ONLY DeviceError GPUMallocArray(cudaArray_t *array, const CudaParams &params, unsigned width, unsigned height, unsigned flags = 0);
-    AX_HOST_ONLY DeviceError GPUFreeArray(cudaArray_t array);
-    AX_HOST_ONLY DeviceError GPUMemcpy2DToArray(
+    AX_DEVICE_CALLABLE void GPUMalloc(void **ptr, std::size_t size);
+    AX_DEVICE_CALLABLE void GPUFree(void *ptr);
+    AX_HOST_ONLY void GPUMallocManaged(void **ptr, std::size_t, const CudaParams &params);
+    AX_HOST_ONLY void GPUMemcpy(const void *ptr_source, void *ptr_dest, std::size_t byte_count, const CudaParams &params);
+    AX_HOST_ONLY void GPUMallocArray(cudaArray_t *array, const CudaParams &params, unsigned width, unsigned height, unsigned flags = 0);
+    AX_HOST_ONLY void GPUFreeArray(cudaArray_t array);
+    AX_HOST_ONLY void GPUMemcpy2DToArray(
         cudaArray_t array, size_t wOffset, size_t hOffset, const void *src, size_t spitch, size_t width, size_t height, const CudaParams &params);
-    AX_HOST_ONLY DeviceError GPUHostRegister(void *ptr, std::size_t size_bytes, unsigned flags);
-    AX_HOST_ONLY DeviceError GPUHostGetDevicePointer(void **ptr_device, void *ptr_host, unsigned flags);
-    AX_HOST_ONLY DeviceError GPUHostUnregister(void *ptr_host);
+    AX_HOST_ONLY void GPUHostRegister(void *ptr, std::size_t size_bytes, unsigned flags);
+    AX_HOST_ONLY void GPUHostGetDevicePointer(void **ptr_device, void *ptr_host, unsigned flags);
+    AX_HOST_ONLY void GPUHostUnregister(void *ptr_host);
     /* Textures */
-    AX_HOST_ONLY DeviceError GPUCreateTextureObject(cudaTextureObject_t *tex, const CudaParams &params, bool use_resc_view = false);
-    AX_HOST_ONLY DeviceError GPUDestroyTextureObject(cudaTextureObject_t texture_object);
+    AX_HOST_ONLY void GPUCreateTextureObject(cudaTextureObject_t *tex, const CudaParams &params, bool use_resc_view = false);
+    AX_HOST_ONLY void GPUDestroyTextureObject(cudaTextureObject_t texture_object);
 
     /* Synchronizations*/
-    AX_HOST_ONLY DeviceError GPUDeviceSynchronize();
+    AX_HOST_ONLY void GPUDeviceSynchronize();
   };
 
 }  // namespace ax_cuda
