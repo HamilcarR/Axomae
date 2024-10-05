256f366d41d61bae9a9a4e087c6583576ab6bf96
bensalem amine <bensalem.amine321@gmail.com>
2024-10-05 10:12:46 +0100

    Abstraction over cuda textures for easy texture creation.

--- corelib/internal/device/gpgpu/device_resource_data.h
+++ corelib/internal/device/gpgpu/device_resource_data.h
@@ -2,31 +2,13 @@
 #define DEVICE_RESOURCE_DATA_H
 
 #include "DeviceError.h"
-#include <any>
 
-namespace device::gpgpu::resource {
+namespace device::gpgpu {
 
-  struct GPU_resource {
+  struct GPU_query_result {
     void *device_ptr{};
     DeviceError error_status;
   };
 
-  enum TEXTURE_TYPE { FLOAT = 0 };
-
-  struct texture_channel_descriptor {
-    int bits_size_x, bits_size_y, bits_size_z, bits_size_a;
-    TEXTURE_TYPE texture_type;
-  };
-
-  struct GPU_array {
-    std::any array;
-  };
-
-  struct GPU_texture {
-    std::any texture_object;
-    std::any array_object;
-    texture_channel_descriptor descriptor;
-  };
-
-}  // namespace device::gpgpu::resource
+}  // namespace device::gpgpu
 #endif
--- corelib/nova/integrator/megakernel.cu
+++ corelib/nova/integrator/megakernel.cu
@@ -5,10 +5,12 @@
 #include "internal/common/math/math_texturing.h"
 #include "internal/debug/Logger.h"
 #include "internal/device/gpgpu/cuda/CudaDevice.h"
-#include "internal/device/gpgpu/device_resource_interface.h"
+#include "internal/device/gpgpu/device_transfer_interface.h"
 #include "internal/device/gpgpu/device_utils.h"
 #include "internal/device/gpgpu/kernel_launch_interface.h"
 #include "manager/NovaResourceManager.h"
+
+namespace resrc = device::gpgpu;
 
 /* Serves only as a baseline for performance to compare against */
 namespace nova {
@@ -31,28 +33,45 @@ namespace nova {
       }
     }
   }  // namespace gpu
+
+  static void setup_descriptors(resrc::texture_descriptor &tex_desc, resrc::resource_descriptor &resrc_desc) {
+    resrc::channel_format &ch_format = tex_desc.channel_descriptor;
+    ch_format.bits_size_x = 32;
+    ch_format.bits_size_y = 32;
+    ch_format.bits_size_z = 32;
+    ch_format.bits_size_a = 32;
+    ch_format.format_type = resrc::FLOAT;
+
+    tex_desc.filter_mode = resrc::FILTER_LINEAR;
+    tex_desc.read_mode = resrc::READ_ELEMENT_TYPE;
+    tex_desc.address_mode[0] = tex_desc.address_mode[1] = resrc::ADDRESS_BORDER;
+    tex_desc.normalized_coords = true;
+    resrc_desc.resource_buffer_descriptors.res.array.array = nullptr;
+    resrc_desc.type = resrc::RESOURCE_ARRAY;
+  }
+
   void launch_gpu_kernel(HdrBufferStruct *buffers,
                          unsigned screen_width,
                          unsigned screen_height,
                          NovaRenderEngineInterface *engine_interface,
                          nova::nova_eng_internals &nova_internals) {
+
     if (ax_cuda::utils::cuda_info_device().empty()) {
       LOGS("No suitable gpu detected.");
       nova_internals.exception_manager->addError(nova::exception::GENERAL_GPU_ERROR);
       return;
     }
-    namespace resrc = device::gpgpu::resource;
+
     const NovaResourceManager *resource_manager = nova_internals.resource_manager;
     const texturing::TextureRawData image_texture = resource_manager->getEnvmapData();
     std::size_t screen_size = screen_width * screen_height * buffers->channels * sizeof(float);
-    resrc::texture_channel_descriptor desc{};
-    desc.bits_size_x = 32;
-    desc.bits_size_y = 32;
-    desc.bits_size_z = 32;
-    desc.bits_size_a = 32;
-    desc.texture_type = resrc::FLOAT;
-    resrc::GPU_texture texture_resrc = resrc::create_texture((const void *)image_texture.raw_data, image_texture.width, image_texture.height, desc);
-    resrc::GPU_resource draw_buffer = resrc::allocate_buffer(screen_size);
+
+    resrc::texture_descriptor tex_desc{};
+    resrc::resource_descriptor res_desc{};
+    setup_descriptors(tex_desc, res_desc);
+    resrc::GPU_texture texture_resrc = resrc::create_texture(
+        (const void *)image_texture.raw_data, image_texture.width, image_texture.height, tex_desc, res_desc);
+    resrc::GPU_query_result draw_buffer = resrc::allocate_buffer(screen_size);
     AXCUDA_ERROR_CHECK(draw_buffer.error_status);
 
     kernel_argpack_t argpack;
