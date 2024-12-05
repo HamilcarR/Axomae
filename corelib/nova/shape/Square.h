310c928d1c135a2915f963ae4789d6da563d49b3
Amine Bensalem <bensalem.amine321@gmail.com>
2024-12-05 13:16:39 +0100

    Refactor: Shape/Primitive modules compatibility with cuda

--- corelib/nova/shape/Square.h
+++ corelib/nova/shape/Square.h
@@ -14,24 +14,18 @@ namespace nova::shape {
     glm::vec3 center{};
 
    public:
-    explicit Square(const glm::vec3 &origin_) : origin(origin_) {}
+    CLASS_DCM(Square)
+
+    ax_device_callable explicit Square(const glm::vec3 &origin_);
     /**
      * side_w and side_h must be perpendicular.
      * Constructor will normalize the normal.
      **/
-    Square(const glm::vec3 &origin_, const glm::vec3 &side_w_, const glm::vec3 &side_h_) : origin(origin_), side_w(side_w_), side_h(side_h_) {
-      normal = glm::normalize(glm::cross(side_w, side_h));
-      center = (side_h + side_w) * 0.5f;
-    }
-    ~Square() = default;
-    Square(const Square &other) = default;
-    Square(Square &&other) noexcept = default;
-    Square &operator=(const Square &other) = default;
-    Square &operator=(Square &&other) noexcept = default;
+    ax_device_callable Square(const glm::vec3 &origin_, const glm::vec3 &side_w_, const glm::vec3 &side_h_);
 
-    bool hit(const Ray &ray, float tmin, float tmax, hit_data &data, base_options *user_options) const;
-    ax_no_discard glm::vec3 centroid() const { return center; }
-    ax_no_discard geometry::BoundingBox computeAABB() const;
+    ax_device_callable bool hit(const Ray &ray, float tmin, float tmax, hit_data &data, base_options *user_options) const;
+    ax_device_callable ax_no_discard glm::vec3 centroid() const { return center; }
+    ax_device_callable ax_no_discard geometry::BoundingBox computeAABB() const;
   };
 }  // namespace nova::shape
 
