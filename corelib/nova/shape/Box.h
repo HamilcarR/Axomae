d2c627b3326085c9e004df3cd0e9f92861c61590
Amine Bensalem <bensalem.amine321@gmail.com>
2024-12-05 13:16:39 +0100

    Refactor: Shape/Primitive modules compatibility with cuda

--- corelib/nova/shape/Box.h
+++ corelib/nova/shape/Box.h
@@ -10,24 +10,20 @@ namespace nova::shape {
     geometry::BoundingBox aabb;
 
    public:
-    Box() = default;
-    Box(const glm::vec3 &min_coords, const glm::vec3 &max_coords);
-    explicit Box(const std::vector<float> &vertices);
-    explicit Box(const geometry::BoundingBox &aabb);
-    ~Box() = default;
-    Box(const Box &other) = default;
-    Box(Box &&other) noexcept = default;
-    Box &operator=(const Box &other) = default;
-    Box &operator=(Box &&other) noexcept = default;
+    CLASS_DCM(Box)
+
+    ax_device_callable Box(const glm::vec3 &min_coords, const glm::vec3 &max_coords);
+    ax_device_callable explicit Box(const std::vector<float> &vertices);
+    ax_device_callable explicit Box(const geometry::BoundingBox &aabb);
 
     /**
      * @brief tmin must be initialized to a small value (0.f) while tmax should be set at a highest value.
      * Use camera near and far .
      */
-    ax_no_discard bool hit(const Ray &ray, float tmin, float tmax, hit_data &data, base_options *user_options) const;
-    ax_no_discard glm::vec3 centroid() const { return aabb.getPosition(); }
-    ax_no_discard const glm::vec3 &getPosition() const { return aabb.getPosition(); }
-    ax_no_discard geometry::BoundingBox computeAABB() const { return aabb; }
+    ax_device_callable ax_no_discard bool hit(const Ray &ray, float tmin, float tmax, hit_data &data, base_options *user_options) const;
+    ax_device_callable ax_no_discard glm::vec3 centroid() const { return aabb.getPosition(); }
+    ax_device_callable ax_no_discard const glm::vec3 &getPosition() const { return aabb.getPosition(); }
+    ax_device_callable ax_no_discard geometry::BoundingBox computeAABB() const { return aabb; }
   };
 }  // namespace nova::shape
 
