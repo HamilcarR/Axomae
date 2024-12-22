e1714705f975ba0372ec6f7280eea6df14ad1007
Amine Bensalem <bensalem.amine321@gmail.com>
2024-12-22 20:51:47 +0100

    Refactor: Renamed cuda macros for better readability

--- corelib/internal/device/gpgpu/cuda/cuda_macros.h
+++ corelib/internal/device/gpgpu/cuda/cuda_macros.h
@@ -8,17 +8,19 @@ constexpr unsigned AX_GPU_MAX_BLOCKS = 65535;
 #define ax_device_thread_idx_y (blockDim.y * blockIdx.y + threadIdx.y)
 #define ax_device_thread_idx_z (blockDim.z * blockIdx.z + threadIdx.z)
 
-#define ax_grid_dim_x (gridDim.x * blockDim.x)
-#define ax_grid_dim_y (gridDim.y * blockDim.y)
-#define ax_grid_dim_z (gridDim.z * blockDim.z)
+#define ax_device_grid_dim_x (gridDim.x * blockDim.x)
+#define ax_device_grid_dim_y (gridDim.y * blockDim.y)
+#define ax_device_grid_dim_z (gridDim.z * blockDim.z)
 
-#define ax_linearCM2D_idx (ax_device_thread_idx_x * ax_grid_dim_y + ax_device_thread_idx_y)
-#define ax_linearRM2D_idx (ax_device_thread_idx_y * ax_grid_dim_x + ax_device_thread_idx_x)
-#define ax_linearCM3D_idx (ax_device_thread_idx_x * ax_grid_dim_z * ax_grid_dim_y + ax_device_thread_idx_y * ax_grid_dim_z + ax_device_thread_idx_z)
-#define ax_linearRM3D_idx (ax_device_thread_idx_z * ax_grid_dim_x * ax_grid_dim_y + ax_device_thread_idx_y * ax_grid_dim_x + ax_device_thread_idx_x)
+#define ax_device_linearCM2D_idx (ax_device_thread_idx_x * ax_device_grid_dim_y + ax_device_thread_idx_y)
+#define ax_device_linearRM2D_idx (ax_device_thread_idx_y * ax_device_grid_dim_x + ax_device_thread_idx_x)
+#define ax_device_linearCM3D_idx \
+  (ax_device_thread_idx_x * ax_device_grid_dim_z * ax_device_grid_dim_y + ax_device_thread_idx_y * ax_device_grid_dim_z + ax_device_thread_idx_z)
+#define ax_device_linearRM3D_idx \
+  (ax_device_thread_idx_z * ax_device_grid_dim_x * ax_device_grid_dim_y + ax_device_thread_idx_y * ax_device_grid_dim_x + ax_device_thread_idx_x)
 
-#define ax_gpu_lane_id (ax_linearCM3D_idx % AX_GPU_WARP_SIZE)
-#define ax_gpu_warp_id (ax_linearCM3D_idx / AX_GPU_WARP_SIZE)
+#define ax_device_lane_id (ax_device_linearCM3D_idx % AX_GPU_WARP_SIZE)
+#define ax_device_warp_id (ax_device_linearCM3D_idx / AX_GPU_WARP_SIZE)
 
 /* Synchronization */
 #define ax_gpu_syncthread __syncthreads()
--- corelib/nova/gpu/GPURandomGenerator.cpp
+++ corelib/nova/gpu/GPURandomGenerator.cpp
@@ -16,7 +16,7 @@ namespace math::random {
       : seed(seed_), state_buffer_size(size), device_curand_states(curand_states_buffer) {}
 
   ax_device_only int GPUPseudoRandomGenerator::nrandi(int min, int max) {
-    uint64_t idx = ax_linearCM3D_idx;
+    uint64_t idx = ax_device_linearCM3D_idx;
     if (idx >= state_buffer_size)
       idx = 0;
     curandState_t &state = device_curand_states[idx];
@@ -25,7 +25,7 @@ namespace math::random {
   }
 
   ax_device_only float GPUPseudoRandomGenerator::nrandf(float min, float max) {
-    uint64_t idx = ax_linearCM3D_idx;
+    uint64_t idx = ax_device_linearCM3D_idx;
     if (idx >= state_buffer_size)
       idx = 0;
     curandState &state = device_curand_states[idx];
@@ -38,7 +38,7 @@ namespace math::random {
   ax_device_only bool GPUPseudoRandomGenerator::randb() { return nrandi(0, 1) == 1; }
 
   ax_kernel void kernel_init_pseudo(curandState_t *states, uint64_t seed) {
-    uint64_t idx = ax_linearCM3D_idx;
+    uint64_t idx = ax_device_linearCM3D_idx;
     curand_init(seed % idx, 0, idx, &states[idx]);
   }
 
@@ -65,7 +65,7 @@ namespace math::random {
   }
 
   ax_device_only int GPUQuasiRandomGenerator::nrandi(int min, int max) {
-    uint64_t idx = ax_linearCM3D_idx * dimension;
+    uint64_t idx = ax_device_linearCM3D_idx * dimension;
     if (idx >= state_buffer_size)
       idx = 0;
     curandStateScrambledSobol32_t &state = device_curand_states[idx];
@@ -75,7 +75,7 @@ namespace math::random {
   }
 
   ax_device_only float GPUQuasiRandomGenerator::nrandf(float min, float max) {
-    uint64_t idx = ax_linearCM3D_idx * dimension;
+    uint64_t idx = ax_device_linearCM3D_idx * dimension;
     if (idx >= state_buffer_size)
       idx = 0;
     curandStateScrambledSobol32_t &state = device_curand_states[idx];
@@ -85,10 +85,8 @@ namespace math::random {
   }
 
   ax_device_only glm::vec3 GPUQuasiRandomGenerator::nrand3f(float min, float max) {
-    uint64_t idx = ax_linearCM2D_idx * dimension;
-
+    uint64_t idx = ax_device_linearCM3D_idx * dimension;
     glm::vec3 eval{};
-
     eval.x = to_interval(min, max, curand_uniform(&device_curand_states[idx]));
     eval.y = to_interval(min, max, curand_uniform(&device_curand_states[idx + 1]));
     eval.z = to_interval(min, max, curand_uniform(&device_curand_states[idx + 2]));
@@ -101,9 +99,9 @@ namespace math::random {
                                   curandDirectionVectors32_t *vectors32,
                                   uint32_t *scramble,
                                   unsigned dimension) {
-    uint64_t idx = ax_linearCM2D_idx * dimension;
+    uint64_t idx = ax_device_linearCM3D_idx * dimension;
     for (int i = 0; i < dimension; i++) {
-      curand_init(vectors32[i], scramble[i], ax_device_thread_idx_x * ax_device_thread_idx_y, &states[idx + i]);
+      curand_init(vectors32[i], scramble[i], ax_device_linearRM2D_idx, &states[idx + i]);
     }
   }
 
