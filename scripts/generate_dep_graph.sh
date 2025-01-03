9fbf1e8c59f3c5ace2b1e1e3d0dbdecfff10175b
Amine Bensalem <bensalem.amine321@gmail.com>
2025-01-03 11:45:24 +0100

    Build: Fixed build on GLM compilation failure for GCC 13.x

--- scripts/generate_dep_graph.sh
+++ scripts/generate_dep_graph.sh
@@ -3,5 +3,5 @@
 # Run from root of project
 mkdir -p ../build_metadata/dependencies 
 cd ../build_metadata/dependencies
-cmake --graphviz=dependencies.dot ../../Axomae 
+cmake -DAXOMAE_FROMSOURCE_QT_BUILD=OFF -DAXOMAE_USE_CUDA=OFF --graphviz=dependencies.dot ../../Axomae 
 dot -Tpng dependencies.dot -o dependencies.png
