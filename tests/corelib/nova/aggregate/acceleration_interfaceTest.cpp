0df6350f9606d68fcb5bc9eb0dd24d26cba0afc0
Amine Bensalem <bensalem.amine321@gmail.com>
2025-02-19 06:01:42 +0100

    Build: Added a non gpu build

---
+++ tests/corelib/nova/aggregate/acceleration_interfaceTest.cpp
@@ -0,0 +1,13 @@
+#include "aggregate/acceleration_interface.h"
+#include "Test.h"
+
+/* Creates a primitive_aggregate_data_s structure */
+class GeometryCreator {
+
+ public:
+  GeometryCreator() {}
+
+ private:
+};
+
+TEST(native_bvh_test, build) {}
