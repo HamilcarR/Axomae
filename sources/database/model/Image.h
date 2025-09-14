9097c441b0681419dcc5f88152272f564b22beaf
Amine Bensalem <bensalem.amine321@gmail.com>
2025-09-14 06:50:06 +0100

    Feature: Added more Image storage types
    - Added Image storage for managed resources and host only resources.
     - Texture Viewers can now display a Nova RenderBuffer.

--- sources/database/model/Image.h
+++ sources/database/model/Image.h
@@ -5,6 +5,7 @@
 #include "Thumbnail.h"
 #include <QPixmap>
 #include <internal/common/axstd/managed_buffer.h>
+#include <internal/common/axstd/span.h>
 #include <memory>
 #include <string>
 #include <type_traits>
@@ -38,6 +39,65 @@ namespace image {
     void clear() override { internal_data.clear(); }
   };
 
+  template<class T>
+  class ImageHolderDataHostStorage : public ImageHolderDataStorageInterface<T> {
+   private:
+    std::vector<T> internal_data{};
+
+   public:
+    ImageHolderDataHostStorage() = default;
+
+    template<class COLLECTION, typename = std::enable_if_t<std::is_convertible_v<typename COLLECTION::value_type, T>>>
+    ImageHolderDataHostStorage(const COLLECTION &buffer) : internal_data(buffer.begin(), buffer.end()) {}
+
+    T *data() override { return internal_data.data(); }
+
+    const T *data() const override { return internal_data.data(); }
+
+    std::size_t size() const override { return internal_data.size(); }
+
+    T &operator[](std::size_t index) override { return internal_data[index]; }
+
+    const T &operator[](std::size_t index) const override { return internal_data[index]; }
+
+    void reserve(std::size_t size) override { internal_data.reserve(size); }
+
+    void resize(std::size_t size, const T &value = T()) override { internal_data.resize(size, value); }
+
+    void clear() override { internal_data.clear(); }
+  };
+
+  template<class T>
+  class ImageViewDataStorage : public ImageHolderDataStorageInterface<T> {
+   private:
+    axstd::span<T> internal_data{};
+
+   public:
+    ImageViewDataStorage() = default;
+
+    ImageViewDataStorage(axstd::span<T> span) : internal_data(span) {}
+
+    T *data() override { return internal_data.data(); }
+
+    const T *data() const override { return internal_data.data(); }
+
+    std::size_t size() const override { return internal_data.size(); }
+
+    T &operator[](std::size_t index) override { return internal_data[index]; }
+
+    const T &operator[](std::size_t index) const override { return internal_data[index]; }
+
+    void reserve(std::size_t) override {}
+
+    void resize(std::size_t, const T & = T()) override {}
+
+    void clear() override { internal_data = axstd::span<T>(); }
+  };
+
+  struct DeviceHolder {};
+  struct HostHolder {};
+  struct ViewHolder {};
+
   template<class TYPE>
   class ImageHolder {
    private:
@@ -46,13 +106,18 @@ namespace image {
    public:
     Metadata metadata{};
 
-    ImageHolder() : internal_data(std::make_unique<ImageHolderDataDeviceStorage<TYPE>>()) {}
+    ImageHolder() : internal_data(std::make_unique<ImageHolderDataHostStorage<TYPE>>()) {}
 
     ImageHolder(std::unique_ptr<ImageHolderDataStorageInterface<TYPE>> dep) : internal_data(std::move(dep)) {}
 
-    template<class COLLECTION>
-    ImageHolder(const COLLECTION &img, const image::Metadata &meta)
+    ImageHolder(const axstd::managed_vector<TYPE> &img, const image::Metadata &meta)
         : internal_data(std::make_unique<ImageHolderDataDeviceStorage<TYPE>>(img)), metadata(meta) {}
+
+    ImageHolder(const std::vector<TYPE> &img, const image::Metadata &meta)
+        : internal_data(std::make_unique<ImageHolderDataHostStorage<TYPE>>(img)), metadata(meta) {}
+
+    ImageHolder(const axstd::span<TYPE> &img, const image::Metadata &meta)
+        : internal_data(std::make_unique<ImageViewDataStorage<TYPE>>(img)), metadata(meta) {}
 
     virtual ~ImageHolder() = default;
 
@@ -101,37 +166,39 @@ namespace image {
 
    public:
     Thumbnail icon;
-
-   public:
     ThumbnailImageHolder() = default;
+
+    ThumbnailImageHolder(const axstd::span<TYPE> &assign,
+                         const image::Metadata &_metadata_,
+                         controller::ProgressStatus *status,
+                         bool generate_thumbnail = true)
+        : ImageHolder<TYPE>(assign, _metadata_) {
+      if (generate_thumbnail)
+        icon = Thumbnail(std::vector<TYPE>(BASETYPE::data().begin(), BASETYPE::data().end()), _metadata_, status);
+    }
+
+    ThumbnailImageHolder(const axstd::managed_vector<TYPE> &assign,
+                         const image::Metadata &_metadata_,
+                         controller::ProgressStatus *status,
+                         bool generate_thumbnail = true)
+        : ImageHolder<TYPE>(assign, _metadata_) {
+      if (generate_thumbnail)
+        icon = Thumbnail(std::vector<TYPE>(BASETYPE::data().begin(), BASETYPE::data().end()), _metadata_, status);
+    }
+
     ThumbnailImageHolder(const std::vector<TYPE> &assign,
                          const image::Metadata &_metadata_,
                          controller::ProgressStatus *status,
-                         bool generate_thumbnail = true);
+                         bool generate_thumbnail = true)
+        : ImageHolder<TYPE>(assign, _metadata_) {
+      if (generate_thumbnail)
+        icon = Thumbnail(std::vector<TYPE>(BASETYPE::data().begin(), BASETYPE::data().end()), _metadata_, status);
+    }
 
-    const QPixmap &thumbnail();
-    std::string name();
+    const QPixmap &thumbnail() { return icon.getIcon(); }
+
+    std::string name() { return BASETYPE::metadata.name; }
   };
-
-  template<class T>
-  ThumbnailImageHolder<T>::ThumbnailImageHolder(const std::vector<T> &assign,
-                                                const image::Metadata &_metadata_,
-                                                controller::ProgressStatus *status,
-                                                bool generate_thumbnail)
-      : ImageHolder<T>(assign, _metadata_) {
-    if (generate_thumbnail)
-      icon = Thumbnail(std::vector<T>(BASETYPE::data().begin(), BASETYPE::data().end()), _metadata_, status);
-  }
-
-  template<class T>
-  const QPixmap &ThumbnailImageHolder<T>::thumbnail() {
-    return icon.getIcon();
-  }
-
-  template<class T>
-  std::string ThumbnailImageHolder<T>::name() {
-    return BASETYPE::metadata.name;
-  }
 
   /*******************************************************************************************************************************************/
 
