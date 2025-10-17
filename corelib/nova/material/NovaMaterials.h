c56a432164fdf6cf0aa754813a7a22b4c6b825c4
Amine Bensalem <bensalem.amine321@gmail.com>
2025-10-17 18:09:48 +0100

    Feature: Implemented conversion frame between rendering spaces

--- corelib/nova/material/NovaMaterials.h
+++ corelib/nova/material/NovaMaterials.h
@@ -5,8 +5,10 @@
 #include "glm/fwd.hpp"
 #include "material_datastructures.h"
 #include "ray/Hitable.h"
+#include "ray/IntersectFrame.h"
 #include "ray/Ray.h"
 #include "sampler/Sampler.h"
+#include "texturing/texture_datastructures.h"
 #include <internal/common/axstd/span.h>
 #include <internal/common/math/math_includes.h>
 #include <internal/common/math/utils_3D.h>
@@ -24,18 +26,20 @@ namespace nova::material {
 
   constexpr float INTERSECT_OFFSET = 1e-4f;
 
-  ax_device_callable_inlined glm::vec3 compute_map_normal(const hit_data &hit_d,
-                                                          const TexturePackSampler &t_pack,
-                                                          const glm::mat3 &tbn,
-                                                          texturing::texture_data_aggregate_s &sample_data) {
-    const glm::vec3 map_normal = t_pack.normal(hit_d.u, hit_d.v, sample_data) * 2.f - 1.f;
-    return glm::normalize(tbn * map_normal);
-  }
-
-  ax_device_callable_inlined glm::vec3 hemi_sample(const glm::mat3 &tbn, sampler::SamplerInterface &sampler) {
+  ax_device_callable_inlined glm::vec3 hemi_sample(sampler::SamplerInterface &sampler) {
     float ret[2] = {};
     sampler.sample2D(ret);
-    return tbn * bxdf::hemisphere_sample_uniform(ret);
+    return bxdf::hemisphere_sample_uniform(ret);
+  }
+
+  ax_device_callable_inlined glm::vec3 compute_local_normal(float u,
+                                                            float v,
+                                                            const TexturePackSampler &t_pack,
+                                                            texturing::texture_data_aggregate_s &sample_data) {
+    return t_pack.normal(u, v, sample_data) * 2.f - 1.f;
+  }
+  ax_device_callable_inlined glm::vec3 compute_world_normal(const glm::vec3 &local_normal, const IntersectFrame &shading_frame) {
+    return glm::normalize(shading_frame.localToWorld(local_normal));
   }
 
   class NovaDiffuseMaterial {
@@ -51,16 +55,18 @@ namespace nova::material {
         const Ray & /*in*/, Ray &out, hit_data &hit_d, sampler::SamplerInterface &sampler, shading_data_s &mat_ctx) const {
       AX_ASSERT_NOTNULL(mat_ctx.texture_aggregate);
       TexturePackSampler texture_pack_sampler(t_pack);
-      const glm::mat3 tbn = math::geometry::construct_tbn(hit_d.normal, hit_d.tangent, hit_d.bitangent);
-      glm::vec3 normal = compute_map_normal(hit_d, texture_pack_sampler, tbn, *mat_ctx.texture_aggregate);
-      out.direction = hemi_sample(tbn, sampler);
-      out.origin = hit_d.position + hit_d.normal * INTERSECT_OFFSET;
+      const IntersectFrame &shading_frame = hit_d.shading_frame;
+
+      glm::vec3 perturbed_local_normal = compute_local_normal(hit_d.u, hit_d.v, texture_pack_sampler, *mat_ctx.texture_aggregate);
+      glm::vec3 perturbed_world_normal = compute_world_normal(perturbed_local_normal, shading_frame);
+      glm::vec3 geometric_world_normal = shading_frame.getNormal();
+      out.direction = glm::normalize(shading_frame.localToWorld(hemi_sample(sampler)));
+      out.origin = hit_d.position + geometric_world_normal * INTERSECT_OFFSET;
 
       mat_ctx.texture_aggregate->geometric_data.sampling_vector = hit_d.position;
       hit_d.attenuation = texture_pack_sampler.albedo(hit_d.u, hit_d.v, *mat_ctx.texture_aggregate);
-      hit_d.normal = normal;
       hit_d.emissive = texture_pack_sampler.emissive(hit_d.u, hit_d.v, *mat_ctx.texture_aggregate);
-      return glm::dot(normal, out.direction) > 0;
+      return glm::dot(geometric_world_normal, out.direction) > 0;
     }
   };
 
@@ -79,22 +85,23 @@ namespace nova::material {
     ax_device_callable_inlined bool scatter(
         const Ray &in, Ray &out, hit_data &hit_d, sampler::SamplerInterface &sampler, shading_data_s &mat_ctx) const {
       AX_ASSERT_NOTNULL(mat_ctx.texture_aggregate);
-
       TexturePackSampler texture_pack_sampler(t_pack);
-      const glm::mat3 tbn = math::geometry::construct_tbn(hit_d.normal, hit_d.tangent, hit_d.bitangent);
-      glm::vec3 normal = compute_map_normal(hit_d, texture_pack_sampler, tbn, *mat_ctx.texture_aggregate);
-      glm::vec3 reflected = glm::reflect(in.direction, normal);
+      const IntersectFrame &shading_frame = hit_d.shading_frame;
+
+      glm::vec3 perturbed_shading_normal = compute_local_normal(hit_d.u, hit_d.v, texture_pack_sampler, *mat_ctx.texture_aggregate);
+      glm::vec3 perturbed_world_normal = compute_world_normal(perturbed_shading_normal, shading_frame);
+      glm::vec3 geometric_world_normal = shading_frame.getNormal();
+      glm::vec3 reflected = glm::reflect(in.direction, perturbed_world_normal);
       AX_ASSERT(!ISNAN(reflected), "");
 
-      out.origin = hit_d.position + hit_d.normal * INTERSECT_OFFSET;
-      out.direction = DENAN(glm::normalize(reflected + hemi_sample(tbn, sampler) * fuzz), glm::normalize(reflected));
+      out.origin = hit_d.position + geometric_world_normal * INTERSECT_OFFSET;
+      glm::vec3 generated_direction_vector = shading_frame.localToWorld(hemi_sample(sampler));
+      out.direction = DENAN(glm::normalize(reflected + generated_direction_vector * fuzz), glm::normalize(reflected));
 
       mat_ctx.texture_aggregate->geometric_data.sampling_vector = hit_d.position;
       hit_d.attenuation = texture_pack_sampler.albedo(hit_d.u, hit_d.v, *mat_ctx.texture_aggregate);
       hit_d.emissive = texture_pack_sampler.emissive(hit_d.u, hit_d.v, *mat_ctx.texture_aggregate);
-      hit_d.normal = normal;
-
-      return glm::dot(normal, out.direction) > 0;
+      return glm::dot(geometric_world_normal, out.direction) > 0;
     }
   };
 
@@ -130,11 +137,15 @@ namespace nova::material {
     ax_device_callable_inlined bool scatter(
         const Ray &in, Ray &out, hit_data &hit_d, sampler::SamplerInterface &sampler, shading_data_s &mat_ctx) const {
       TexturePackSampler texture_pack_sampler(t_pack);
-      const glm::mat3 tbn = math::geometry::construct_tbn(hit_d.normal, hit_d.tangent, hit_d.bitangent);
-      glm::vec3 perturbed_normal = compute_map_normal(hit_d, texture_pack_sampler, tbn, *mat_ctx.texture_aggregate);
-      glm::vec3 original_perturbed_normal = perturbed_normal;
+      const IntersectFrame &shading_frame = hit_d.shading_frame;
+
+      glm::vec3 perturbed_shading_normal = compute_local_normal(hit_d.u, hit_d.v, texture_pack_sampler, *mat_ctx.texture_aggregate);
+      glm::vec3 perturbed_world_normal = compute_world_normal(perturbed_shading_normal, shading_frame);
+      glm::vec3 geometric_world_normal = shading_frame.getNormal();
+
+      glm::vec3 original_perturbed_world_normal = perturbed_world_normal;
       glm::vec3 direction = glm::normalize(in.direction);
-      const glm::vec3 reflected = glm::reflect(direction, perturbed_normal);
+      const glm::vec3 reflected = glm::reflect(direction, perturbed_world_normal);
       out.origin = hit_d.position;
       AX_ASSERT_NOTNULL(mat_ctx.texture_aggregate);
       mat_ctx.texture_aggregate->geometric_data.sampling_vector = out.origin;
@@ -143,15 +154,15 @@ namespace nova::material {
       float index = eta;
       float reflect_prob = 0.f;
       float cosine = 0.f;
-      if (glm::dot(direction, perturbed_normal) > 0) {
-        original_perturbed_normal = -original_perturbed_normal;
-        cosine = glm::dot(direction, perturbed_normal) * eta / glm::length(direction);
+      if (glm::dot(direction, perturbed_world_normal) > 0) {
+        original_perturbed_world_normal = -original_perturbed_world_normal;
+        cosine = glm::dot(direction, perturbed_world_normal) * eta / glm::length(direction);
       } else {
         index = 1.f / eta;
-        cosine = -glm::dot(direction, perturbed_normal) / glm::length(direction);
+        cosine = -glm::dot(direction, perturbed_world_normal) / glm::length(direction);
       }
       glm::vec3 refracted;
-      if (refract(direction, original_perturbed_normal, index, refracted))
+      if (refract(direction, original_perturbed_world_normal, index, refracted))
         reflect_prob = schlick(cosine, eta);
       else
         reflect_prob = 1.f;
@@ -160,7 +171,6 @@ namespace nova::material {
         out.direction = glm::normalize(reflected);
       else
         out.direction = glm::normalize(refracted);
-      hit_d.normal = perturbed_normal;
       AX_ASSERT(!ISNAN(out.direction), "");
       return true;
     }
---
+++ corelib/nova/ray/IntersectFrame.h
@@ -0,0 +1,56 @@
+#ifndef INTERSECTFRAME_H
+#define INTERSECTFRAME_H
+#include "glm/geometric.hpp"
+#include <internal/common/math/utils_3D.h>
+#include <internal/device/gpgpu/device_macros.h>
+#include <internal/device/gpgpu/device_utils.h>
+
+/**
+ * @brief Provides conversions procedures between local shading space to world space.
+ */
+class IntersectFrame {
+  glm::mat3 tbn{1.f};  // orthonormal frame.
+  glm::mat3 tbn_t{1.f};
+
+ public:
+  ax_device_callable_inlined IntersectFrame() = default;
+
+  /* Recomputes the frame to ensure perfect orthonormality (Gram-Shmidt).*/
+  static ax_device_callable_inlined void orthonormalize(glm::vec3 &t, glm::vec3 &b, glm::vec3 &n) {
+    n = glm::normalize(n);
+    t = glm::normalize(t - glm::dot(t, n) * n);
+    b = glm::normalize(glm::cross(n, t));
+  }
+
+  ax_device_callable_inlined IntersectFrame(const float tangent[3], const float bitangent[3], const float normal[3], bool normalize = false) {
+    glm::vec3 n(normal[0], normal[1], normal[2]);
+    glm::vec3 t(tangent[0], tangent[1], tangent[2]);
+    glm::vec3 b(bitangent[0], bitangent[1], bitangent[2]);
+    if (normalize)
+      orthonormalize(t, b, n);
+    tbn = glm::mat3(t, b, n);
+    tbn_t = glm::transpose(tbn);
+  }
+
+  ax_device_callable_inlined IntersectFrame(const glm::vec3 &tangent,
+                                            const glm::vec3 &bitangent,
+                                            const glm::vec3 &normal,
+                                            bool orthonormalize = false)
+      : IntersectFrame((const float *)&tangent, (const float *)&bitangent, (const float *)&normal, orthonormalize) {}
+
+  ax_device_callable_inlined glm::vec3 localToWorld(const glm::vec3 &vect) const { return tbn * vect; }
+
+  ax_device_callable_inlined glm::vec3 worldToLocal(const glm::vec3 &vect) const { return tbn_t * vect; }
+
+  ax_device_callable_inlined const glm::mat3 &getFrame() const { return tbn; }
+
+  ax_device_callable_inlined const glm::mat3 &getTranspose() const { return tbn_t; }
+
+  ax_device_callable_inlined const glm::vec3 &getNormal() const { return tbn[2]; }
+
+  ax_device_callable_inlined const glm::vec3 &getTangent() const { return tbn[0]; }
+
+  ax_device_callable_inlined const glm::vec3 &getBitangent() const { return tbn[1]; }
+};
+
+#endif
