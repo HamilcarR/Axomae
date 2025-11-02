dc932c7275d14b1bb1bd544cf7f7fe06d90d3e1d
Amine Bensalem <bensalem.amine321@gmail.com>
2025-11-02 03:31:24 +0100

    Fixed self intersection errors on conductor mesh edges

--- corelib/nova/api/material.cpp
+++ corelib/nova/api/material.cpp
@@ -4,6 +4,53 @@
 #include "texturing/NovaTextureInterface.h"
 #include "texturing/nova_texturing.h"
 namespace nova {
+  // Aluminum
+  const glm::vec3 eta_Al = glm::vec3(1.657f, 0.880f, 0.521f);
+  const glm::vec3 k_Al = glm::vec3(9.223f, 6.269f, 4.837f);
+
+  // Copper
+  const glm::vec3 eta_Cu = glm::vec3(0.271f, 0.676f, 1.316f);
+  const glm::vec3 k_Cu = glm::vec3(3.609f, 2.624f, 2.292f);
+
+  // Gold
+  const glm::vec3 eta_Au = glm::vec3(0.182f, 0.421f, 1.373f);
+  const glm::vec3 k_Au = glm::vec3(3.424f, 2.345f, 1.770f);
+
+  // Silver
+  const glm::vec3 eta_Ag = glm::vec3(0.155f, 0.143f, 0.135f);
+  const glm::vec3 k_Ag = glm::vec3(4.828f, 3.122f, 2.146f);
+
+  // Iron
+  const glm::vec3 eta_Fe = glm::vec3(2.911f, 2.949f, 2.584f);
+  const glm::vec3 k_Fe = glm::vec3(3.089f, 2.931f, 2.767f);
+
+  // Chromium
+  const glm::vec3 eta_Cr = glm::vec3(3.177f, 3.182f, 2.220f);
+  const glm::vec3 k_Cr = glm::vec3(3.334f, 3.329f, 3.088f);
+
+  // Nickel
+  const glm::vec3 eta_Ni = glm::vec3(1.910f, 1.570f, 1.134f);
+  const glm::vec3 k_Ni = glm::vec3(2.452f, 1.935f, 1.654f);
+
+  // Titanium
+  const glm::vec3 eta_Ti = glm::vec3(2.745f, 2.389f, 1.532f);
+  const glm::vec3 k_Ti = glm::vec3(3.130f, 2.617f, 1.955f);
+
+  // Platinum
+  const glm::vec3 eta_Pt = glm::vec3(2.375f, 2.084f, 1.845f);
+  const glm::vec3 k_Pt = glm::vec3(4.265f, 3.715f, 3.136f);
+
+  // Zinc
+  const glm::vec3 eta_Zn = glm::vec3(1.370f, 0.920f, 0.666f);
+  const glm::vec3 k_Zn = glm::vec3(3.100f, 2.570f, 2.330f);
+
+  // Cobalt
+  const glm::vec3 eta_Co = glm::vec3(2.260f, 1.590f, 1.310f);
+  const glm::vec3 k_Co = glm::vec3(4.980f, 4.430f, 4.090f);
+
+  // Lead
+  const glm::vec3 eta_Pb = glm::vec3(1.910f, 1.830f, 1.440f);
+  const glm::vec3 k_Pb = glm::vec3(3.510f, 3.400f, 3.180f);
 
   class NvMaterial final : public Material {
     TexturePtr albedo;
@@ -143,10 +190,10 @@ namespace nova {
   static nova::material::NovaMaterialInterface assign_random_material(nova::material::texture_pack &tpack, nova::NovaResourceManager &manager) {
     math::random::CPUPseudoRandomGenerator rand_gen;
     nova::material::NovaMaterialInterface mat_ptr{};
-    int r = 2;  // rand_gen.nrandi(0, 2);
+    int r = 0;  // rand_gen.nrandi(0, 1) == 0 ? 0 : 2;
     switch (r) {
       case 0:
-        mat_ptr = manager.getMaterialData().addMaterial<nova::material::NovaConductorMaterial>(tpack, rand_gen.nrandf(0.001, 0.001));
+        mat_ptr = manager.getMaterialData().addMaterial<nova::material::NovaConductorMaterial>(tpack, eta_Co, k_Co);
         break;
       case 1:
         mat_ptr = manager.getMaterialData().addMaterial<nova::material::NovaDielectricMaterial>(tpack, rand_gen.nrandf(1.5, 2.4));
@@ -155,7 +202,7 @@ namespace nova {
         mat_ptr = manager.getMaterialData().addMaterial<nova::material::NovaDiffuseMaterial>(tpack);
         break;
       default:
-        mat_ptr = manager.getMaterialData().addMaterial<nova::material::NovaConductorMaterial>(tpack, 0.004);
+        mat_ptr = manager.getMaterialData().addMaterial<nova::material::NovaDiffuseMaterial>(tpack);
         break;
     }
     return mat_ptr;
